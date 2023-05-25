9a8813693b36e126ec208bbb4e1b8fa366813d81
Florian Zand <florianzand@me.com>
2023-05-25 08:55:32 +0200

    Update QuicklookView.swift

--- Sources/FZUIKit/Classes/AppKit/QuickLookPanel/QuicklookView.swift
+++ Sources/FZUIKit/Classes/AppKit/QuickLookPanel/QuicklookView.swift
@@ -17,7 +17,7 @@ public class QuicklookView: NSView {
     internal var qlPreviewView: QLPreviewView!
     
     /**
-    The style of the view.
+     The style of the view.
      */
     public var style: QLPreviewViewStyle = .normal {
         didSet {
@@ -31,7 +31,7 @@ public class QuicklookView: NSView {
     
     /**
      The item to preview.
-
+     
      Quick Look requires Items you wish to conform to the QLPreviewable protocol. When you set this property, the QuicklookView loads the preview asynchronously. Due to this asynchronous behavior, don’t assume that the preview is ready immediately after assigning it to this property.
      */
     public var item: QLPreviewable? {
@@ -40,8 +40,21 @@ public class QuicklookView: NSView {
     }
     
     /**
+     The url to the file to preview.
+     */
+    public var fileURL: URL? {
+        get { self.item?.previewItemURL }
+        set {
+            if fileURL != newValue {
+                if let newValue = newValue { self.item = QuicklookItem(url: newValue)
+                } else { self.item = nil }
+            }
+        }
+    }
+    
+    /**
      Updates the preview to display the currently previewed item.
-
+     
      When you modify the object that the item property points to, call this method to generate and display the new preview.
      */
     public func refreshItem() {
@@ -51,19 +64,19 @@ public class QuicklookView: NSView {
     
     /**
      A Boolean value that determines whether the preview starts automatically.
-
+     
      Set this property to allow previews of movie files to start playback automatically when displayed.
-    */
+     */
     public var autostarts: Bool {
         get { qlPreviewView.autostarts }
         set { qlPreviewView.autostarts = newValue }
     }
-
+    
     /**
      A Boolean value that determines whether the preview should close when its window closes.
-
+     
      The default value of this property is true, which means that the preview automatically closes when its window closes. If you set this property to false, close the preview by calling the close() method when finished with it. Once you close a QuicklookView, it won’t accept any more preview items.
-    */
+     */
     public var shouldCloseWithWindow: Bool {
         get { qlPreviewView.shouldCloseWithWindow }
         set { qlPreviewView.shouldCloseWithWindow = newValue }
@@ -71,7 +84,7 @@ public class QuicklookView: NSView {
     
     /**
      Closes the view, releasing the current  item.
-
+     
      Once a QuicklookView is closed, it won’t accept any more preview items. You only need to call this method if shouldCloseWithWindow is set to false. If you don’t close a QuicklookView when you are done using it, your app will leak memory.
      */
     public func close() {
@@ -83,7 +96,7 @@ public class QuicklookView: NSView {
      - Parameter item: The item to preview.
      - Parameter style: The desired style for the QuicklookView object.
      - Returns: Returns a QuicklookView object with the designated item and style.
-
+     
      */
     public init(item: QLPreviewable, style: QLPreviewViewStyle = .normal) {
         super.init(frame: .zero)
@@ -95,7 +108,7 @@ public class QuicklookView: NSView {
      - Parameter fileURL: The url to the file to preview.
      - Parameter style: The desired style for the QuicklookView object.
      - Returns: Returns a QuicklookView object with the designated item and style.
-
+     
      */
     public convenience init(fileURL: URL, style: QLPreviewViewStyle = .normal) {
         self.init(item: QuicklookItem(url: fileURL), style: style)
