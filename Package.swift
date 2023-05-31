f2dcdd5b4847c4ab0a1f9d3ba6c0e498c354ad71
Florian Zand <florianzand@me.com>
2023-06-01 01:59:45 +0200

    updated

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "FZUIKit",
-    platforms: [.macOS("10.15.1"), .iOS(.v14)],
+    platforms: [.macOS("10.15.1"), .iOS(.v14), .macCatalyst(.v14), .tvOS(.v14), .watchOS(.v7)],
     products: [
         .library(
             name: "FZUIKit",
