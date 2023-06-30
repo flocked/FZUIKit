808d98c4ec2ae5379d41c7d0ea13cfd14c538ea6
Florian Zand <florianzand@me.com>
2023-06-30 22:40:11 +0200

    updated

---
+++ Sources/FZUIKit/Classes/InnerShadow/DashedBorderLayer.swift
@@ -0,0 +1,98 @@
+//
+//  File.swift
+//  
+//
+//  Created by Florian Zand on 30.06.23.
+//
+
+import Foundation
+#if os(macOS)
+import AppKit
+#elseif canImport(UIKit)
+import UIKit
+#endif
+import FZSwiftUtils
+
+/// A CALayer that displays a inner shadow.
+public class DashedBorderLayer: CALayer {
+    public var borderInsets: NSDirectionalEdgeInsets = .init(0) {
+        didSet {
+            guard oldValue != borderInsets else { return }
+            layoutBorderedLayer()
+        }
+    }
+    
+    public override var borderColor: CGColor? {
+        get { borderedLayer.strokeColor }
+        set { borderedLayer.strokeColor = newValue }
+    }
+    
+    public override var borderWidth: CGFloat {
+        get { borderedLayer.lineWidth }
+        set {
+            guard newValue != borderWidth else { return }
+            borderedLayer.lineWidth = newValue
+            layoutBorderedLayer()
+        }
+    }
+    
+    public var borderDashPattern: [CGFloat]? {
+        get { borderedLayer.lineDashPattern?.compactMap({$0.doubleValue}) }
+        set { borderedLayer.lineDashPattern = newValue as? [NSNumber] }
+    }
+    
+    public var configuration: ContentConfiguration.Border {
+        get { ContentConfiguration.Border(color: self.borderColor?.nsColor, width: self.borderWidth, dashPattern: self.borderDashPattern, insets: self.borderInsets) }
+        set { guard newValue != self.configuration else { return  }
+            self.borderWidth = newValue.width
+            self.borderColor = newValue._resolvedColor?.cgColor
+            self.borderInsets = newValue.insets
+            self.borderDashPattern = newValue.dashPattern
+        }
+    }
+    
+    public override func layoutSublayers() {
+        self.layoutBorderedLayer()
+        super.layoutSublayers()
+    }
+    
+    internal let borderedLayer = CAShapeLayer()
+    
+    internal func layoutBorderedLayer() {
+        let frameSize = CGSize(width: self.frame.size.width-borderInsets.width, height: self.frame.size.height-borderInsets.height)
+        let shapeRect = CGRect(origin: CGPoint(x: borderInsets.leading, y: borderInsets.bottom), size: frameSize)
+        
+        let scale = (shapeRect.size.width-borderWidth)/self.frame.size.width
+        let cornerRadius = self.cornerRadius * scale
+        
+        borderedLayer.bounds = CGRect(.zero, shapeRect.size)
+        borderedLayer.position = CGPoint(x: frameSize.width/2, y: frameSize.height/2)
+        borderedLayer.path = NSUIBezierPath(roundedRect: shapeRect, cornerRadius: cornerRadius).cgPath
+    }
+    
+    public init(configuration: ContentConfiguration.Border) {
+        super.init()
+        self.configuration = configuration
+    }
+        
+    public override init() {
+        super.init()
+        self.sharedInit()
+    }
+    
+    public override init(layer: Any) {
+        super.init(layer: layer)
+        self.sharedInit()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.sharedInit()
+    }
+    
+    internal func sharedInit() {
+        borderedLayer.fillColor = .clear
+        borderedLayer.lineJoin = CAShapeLayerLineJoin.round
+        self.addSublayer(borderedLayer)
+    }
+}
---
+++ Sources/FZUIKit/Classes/InnerShadow/DashedBorderView.swift
@@ -0,0 +1,89 @@
+//
+//  InnerShadowView.swift
+//  FZExtensions
+//
+//  Created by Florian Zand on 03.09.22.
+//
+
+
+#if os(macOS)
+import AppKit
+#elseif canImport(UIKit)
+import UIKit
+#endif
+
+/// A view that displays a inner shadow.
+public class DashedBorderView: NSUIView {
+    internal static let Tag = 435_234_364
+
+    /// The color of the inner shadow.
+    public var borderInsets: NSDirectionalEdgeInsets {
+        get { borderedLayer.borderInsets }
+        set { borderedLayer.borderInsets = newValue }
+    }
+
+    /// The configuration of the inner shadow.
+    public var configuration: ContentConfiguration.Border {
+        get { borderedLayer.configuration }
+        set { borderedLayer.configuration = newValue }
+    }
+    
+    /*
+    public override func layoutSubviews() {
+        innershadowLayer.update()
+        super.layoutSubviews()
+    }
+     */
+        
+    /**
+     Initalizes a inner shadow view with the specified configuration.
+     
+     - Parameters configuration: The configuration of the inner shadow.
+     - Returns: The inner shadow view.
+     */
+    public init(configuration: ContentConfiguration.Border) {
+        super.init(frame: .zero)
+        self.configuration = configuration
+    }
+
+    internal var borderedLayer: DashedBorderLayer {
+#if os(macOS)
+        self.wantsLayer = true
+#endif
+        return self.layer as! DashedBorderLayer
+    }
+    
+    #if os(macOS)
+    override public func makeBackingLayer() -> CALayer {
+        let borderedLayer = DashedBorderLayer()
+     //   shadowLayer.zPosition = FLT_MAX
+        return borderedLayer
+    }
+
+    override public var tag: Int {
+        return Self.Tag
+    }
+    #else
+    override public class var layerClass: AnyClass {
+        return DashedBorderLayer.self
+    }
+    #endif
+        
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        self.sharedInit()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.sharedInit()
+    }
+    
+    internal func sharedInit() {
+#if canImport(UIKit)
+        tag = Self.Tag
+    //    layer.zPosition = .greatestFiniteMagnitude
+#endif
+    }
+}
+
