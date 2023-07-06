5efc63e6ab66f68317ef2ad8ad2a52b9072cdf60
Florian Zand <florianzand@me.com>
2023-07-06 11:34:27 +0200

    Update FirstRespondable.swift

--- Sources/FZUIKit/Extensions/AppKit & UIKit/FirstRespondable.swift
+++ Sources/FZUIKit/Extensions/AppKit & UIKit/FirstRespondable.swift
@@ -10,6 +10,7 @@ import AppKit
 #elseif canImport(UIKit)
 import UIKit
 #endif
+import FZSwiftUtils
 
 /// A protocol that indicating whether the conforming object is the first responder.
 public protocol FirstRespondable {
@@ -21,11 +22,13 @@ public protocol FirstRespondable {
      - Returns: `true if the responder is the first responder; otherwise, `false`.
      */
     var isFirstResponder: Bool { get }
+    @discardableResult func resignFirstResponder() -> Bool
+    @discardableResult func becomeFirstResponder() -> Bool
 }
 
 extension NSUIView: FirstRespondable { }
 extension NSUIViewController: FirstRespondable { }
-
+#if os(macOS)
 public extension FirstRespondable where Self: NSView {
     /**
      Returns a Boolean value indicating whether this object is the first responder.
@@ -37,8 +40,13 @@ public extension FirstRespondable where Self: NSView {
     var isFirstResponder: Bool {
         (self.window?.firstResponder == self)
     }
+    
+    /*
+    @discardableResult func makeFirstResponder() -> Bool {
+        self.window?.makeFirstResponder(self) ?? false
+    }
+     */
 }
- 
 
 public extension FirstRespondable where Self: NSViewController {
     /**
@@ -51,4 +59,88 @@ public extension FirstRespondable where Self: NSViewController {
     var isFirstResponder: Bool {
         (self.view.window?.firstResponder == self)
     }
+    
+    /*
+    @discardableResult func makeFirstResponder() -> Bool {
+        self.view.window?.makeFirstResponder(self) ?? false
+    }
+     */
 }
+
+extension NSView {
+   @discardableResult override open func becomeFirstResponder() -> Bool {
+       if self.acceptsFirstResponder, let window = self.window {
+           if window.firstResponder != self, isChangingFirstResponder == false {
+               isChangingFirstResponder = true
+               window.makeFirstResponder(self)
+               return super.becomeFirstResponder()
+           }
+       }
+       isChangingFirstResponder = false
+       return super.becomeFirstResponder()
+   }
+   
+    @discardableResult override open func resignFirstResponder() -> Bool {
+        if let window = self.window {
+            if window.firstResponder == self, isChangingFirstResponder == false {
+                isChangingFirstResponder = true
+                window.makeFirstResponder(nil)
+                return super.resignFirstResponder()
+            }
+        }
+        isChangingFirstResponder = false
+        return super.resignFirstResponder()
+   }
+   
+   internal var isChangingFirstResponder: Bool {
+       get { getAssociatedValue(key: "NSView_isChangingFirstResponder", object: self, initialValue: false) }
+       set { set(associatedValue: newValue, key: "NSView_isChangingFirstResponder", object: self) }
+   }
+}
+
+extension NSViewController {
+    @discardableResult override open func becomeFirstResponder() -> Bool {
+       if self.acceptsFirstResponder, let window = self.view.window {
+           if window.firstResponder != self, isChangingFirstResponder == false {
+               isChangingFirstResponder = true
+               window.makeFirstResponder(self)
+               return super.becomeFirstResponder()
+           }
+       }
+       isChangingFirstResponder = false
+       return super.becomeFirstResponder()
+   }
+   
+    @discardableResult override open func resignFirstResponder() -> Bool {
+        if let window = self.view.window {
+            if window.firstResponder == self, isChangingFirstResponder == false {
+                isChangingFirstResponder = true
+                window.makeFirstResponder(nil)
+                return super.resignFirstResponder()
+            }
+        }
+        isChangingFirstResponder = false
+        return super.resignFirstResponder()
+   }
+   
+   internal var isChangingFirstResponder: Bool {
+       get { getAssociatedValue(key: "NSViewController_isChangingFirstResponder", object: self, initialValue: false) }
+       set { set(associatedValue: newValue, key: "NSViewController_isChangingFirstResponder", object: self) }
+   }
+}
+#endif
+/*
+#if canImport(UIKit)
+public extension FirstRespondable where Self: UIView {
+    @discardableResult func makeFirstResponder() -> Bool {
+        self.becomeFirstResponder()
+    }
+}
+
+public extension FirstRespondable where Self: UIViewController {
+    @discardableResult func makeFirstResponder() -> Bool {
+        self.becomeFirstResponder()
+    }
+}
+#endif
+*/
