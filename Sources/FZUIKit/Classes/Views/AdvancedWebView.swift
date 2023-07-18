5e40f51008a1308f9827f29f1b5ceaa9afc18ec6
Florian Zand <florianzand@me.com>
2023-07-18 11:18:03 +0200

    Update AdvancedWebView.swift

--- Sources/FZUIKit/Classes/Views/AdvancedWebView.swift
+++ Sources/FZUIKit/Classes/Views/AdvancedWebView.swift
@@ -19,6 +19,32 @@ public class AdvanceWebView: WKWebView {
     /// The handler that returns the current HTTP cookies when the web view finishes loading a website.
     public var cookiesHandler: (([HTTPCookie]) -> ())? = nil
     
+    @available(macOS 11.3, *)
+    /// The handlers for downloading files.
+    struct DownloadHandlers {
+        /// The handler that determines whether a url request should be downloaded.
+        public var shouldDownload: ((URLRequest)->(Bool))? = nil
+        /// The handler that determines the file location of a finished download.
+        public var downloadLocation: ((_ suggestedFilename: String)->(URL?))? = nil
+        /// The handler that gets called whenever a download finishes.
+        public var didFinish: (()->())? = nil
+        /// The handler that gets called whenever a download failed.
+        public var didFail: ((_ error: Error, _ resumeData: Data?)->())? = nil
+    }
+    
+    @available(macOS 11.3, *)
+    /// The handlers for downloading files.
+    var downloadHandlers: DownloadHandlers {
+        get {
+            if _downloadHandlers == nil {
+                _downloadHandlers = DownloadHandlers()
+            }
+            return _downloadHandlers as! DownloadHandlers
+        }
+        set {  _downloadHandlers = newValue }
+    }
+    private var _downloadHandlers: Any? = nil
+
     /// The current url request.
     public var currentRequest: URLRequest? = nil
     
@@ -52,6 +78,16 @@ public class AdvanceWebView: WKWebView {
 
 
 extension AdvanceWebView: WKNavigationDelegate  {
+    @available(macOS 11.3, *)
+    public func webView(_ webView: WKWebView, navigationAction: WKNavigationAction, didBecome download: WKDownload) {
+        download.delegate = self
+    }
+    
+    @available(macOS 11.3, *)
+    public func webView(_ webView: WKWebView, navigationResponse: WKNavigationResponse, didBecome download: WKDownload) {
+        download.delegate = self
+    }
+    
     public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         if self.currentRequest != navigationAction.request {
             self.didFinishLoadingHandler?(navigationAction.request)
@@ -71,8 +107,37 @@ extension AdvanceWebView: WKNavigationDelegate  {
             }
             self.currentHTTPCookies = cookies
         })
-        decisionHandler(.allow)
+       
+        if #available(macOS 11.3, *) {
+            let shouldDownload = downloadHandlers.shouldDownload?(navigationAction.request) ?? false
+            decisionHandler(shouldDownload ? .download : .allow)
+        } else {
+            decisionHandler(.allow)
+        }
     }
 }
 
-
+@available(macOS 11.3, *)
+extension AdvanceWebView: WKDownloadDelegate {
+    public func download(_ download: WKDownload, decideDestinationUsing response: URLResponse, suggestedFilename: String, completionHandler: @escaping (URL?) -> Void) {
+        Swift.print("[AdvanceWebView] download suggestedFilename", suggestedFilename)
+        let url = self.downloadHandlers.downloadLocation?(suggestedFilename) ?? nil
+        completionHandler(url)
+    }
+    
+    public func downloadDidFinish(_ download: WKDownload) {
+        Swift.print("[AdvanceWebView] download didFinish")
+        self.downloadHandlers.didFinish?()
+    }
+    
+    public func download(_ download: WKDownload, didFailWithError error: Error, resumeData: Data?) {
+        Swift.print("[AdvanceWebView] download failed", error)
+        self.downloadHandlers.didFail?(error, resumeData)
+    }
+    
+    public func download(_ download: WKDownload, willPerformHTTPRedirection response: HTTPURLResponse, newRequest request: URLRequest, decisionHandler: @escaping (WKDownload.RedirectPolicy) -> Void) {
+        Swift.print("[AdvanceWebView] download willPerformHTTPRedirection", response, response.url ?? "")
+        decisionHandler(.allow)
+    }
+    
+}
