39f760a4ca7be2844434951a69b7122bb1a50169
Florian Zand <florianzand@me.com>
2023-07-29 03:42:30 +0200

    Create NSDiffableDataSourceSnapshot+.swift

---
+++ Sources/FZUIKit/Extensions/AppKit & UIKit/NSDiffableDataSourceSnapshot+.swift
@@ -0,0 +1,26 @@
+//
+//  NSDiffableDataSourceSnapshot+.swift
+//  
+//
+//  Created by Florian Zand on 29.07.23.
+//
+
+#if os(macOS)
+import AppKit
+#elseif canImport(UIKit)
+import UIKit
+#endif
+
+public extension NSDiffableDataSourceSnapshot {
+    /**
+     Adds the sections and items with the specified identifiers to the snapshot.
+     
+     - Parameters sectionItems: A dictionary of section and item identifiers to add to the snapshot.
+     */
+    mutating func append(_ sectionItems: [SectionIdentifierType: [ItemIdentifierType]]) {
+        self.appendSections(Array(sectionItems.keys))
+        for value in sectionItems {
+            self.appendItems(value.value, toSection: value.key)
+        }
+    }
+}
