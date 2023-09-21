e86ac8844563508d28d723981b70fce36f6d0d35
Florian Zand <florianzand@me.com>
2023-09-21 03:04:46 +0200

    updatedupdated

---
+++ Sources/FZUIKit/Classes/AppKit/Views/CenteredClipView.swift
@@ -0,0 +1,29 @@
+//
+//  CenteredClipView.swift
+//
+//
+//  Created by Florian Zand on 21.09.23.
+//
+
+#if os(macOS)
+import AppKit
+
+/// A `NSClipView` that is centered.
+public class CenteredClipView: NSClipView {
+    public override func constrainBoundsRect(_ proposedBounds: NSRect) -> NSRect {
+        var rect = super.constrainBoundsRect(proposedBounds)
+
+        if let containerView = documentView {
+            if rect.size.width > containerView.frame.size.width {
+                rect.origin.x = (containerView.frame.width - rect.width) / 2
+            }
+
+            if rect.size.height > containerView.frame.size.height {
+                rect.origin.y = (containerView.frame.height - rect.height) / 2
+            }
+        }
+
+        return rect
+    }
+}
+#endif
