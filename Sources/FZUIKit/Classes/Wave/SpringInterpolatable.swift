210834aff017dd293cad612e172855c091c4520b
Florian Zand <florianzand@me.com>
2023-10-08 02:31:44 +0200

    Update SpringInterpolatable.swift

--- Sources/FZUIKit/Classes/Wave/SpringInterpolatable.swift
+++ Sources/FZUIKit/Classes/Wave/SpringInterpolatable.swift
@@ -57,6 +57,7 @@ extension CGFloat: SpringInterpolatable, VelocityProviding { }
 extension CGSize: SpringInterpolatable, VelocityProviding { }
 extension CGPoint: SpringInterpolatable, VelocityProviding { }
 extension CGRect: SpringInterpolatable, VelocityProviding {
+    /*
     public static func updateValue(spring: Spring, value: CGRect, target: CGRect, velocity: CGRect, dt: TimeInterval) -> (value: CGRect, velocity: CGRect) {
         let origin = CGPoint.updateValue(spring: spring, value: value.origin, target: target.origin, velocity: velocity.origin, dt: dt)
         let size = CGSize.updateValue(spring: spring, value: value.size, target: target.size, velocity: velocity.size, dt: dt)
@@ -66,6 +67,7 @@ extension CGRect: SpringInterpolatable, VelocityProviding {
         
         return (newValue, newVelocity)
     }
+    */
 }
 
 extension NSUIColor: SpringInterpolatable, VelocityProviding {
