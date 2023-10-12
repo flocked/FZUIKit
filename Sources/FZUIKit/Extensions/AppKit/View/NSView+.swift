eaf6d32415b27364cc596f4168c65c8c5064ab6d
Florian Zand <florianzand@me.com>
2023-10-12 15:42:58 +0200

    Update NSView+.swift

--- Sources/FZUIKit/Extensions/AppKit/View/NSView+.swift
+++ Sources/FZUIKit/Extensions/AppKit/View/NSView+.swift
@@ -415,11 +415,9 @@ extension NSView {
 
      Using this property turns the view into a layer-backed view. The value can be animated via `animator()`.
      */
-    @objc open dynamic var cornerRadius: CGFloat {
+    @objc internal dynamic var cornerRadius: CGFloat {
         get { layer?.cornerRadius ?? 0.0 }
         set {
-            _cornerRadius = newValue
-            /*
             Swift.print("cornerRadius 0", self, self.maskToBounds)
             let maskToBounds = self.maskToBounds
             wantsLayer = true
@@ -428,24 +426,9 @@ extension NSView {
             // fix for macOS 14.0 bug
             self.maskToBounds = maskToBounds
             Swift.print("cornerRadius 1", self, self.maskToBounds)
-             */
         }
     }
     
-    @objc public dynamic var _cornerRadius: CGFloat {
-        get { layer?.cornerRadius ?? 0.0 }
-        set {
-            Swift.print("cornerRadius 0", self, self.maskToBounds)
-            let maskToBounds = self.maskToBounds
-            wantsLayer = true
-            Self.swizzleAnimationForKey()
-            layer?.cornerRadius = newValue
-            // fix for macOS 14.0 bug
-            self.maskToBounds = maskToBounds
-            Swift.print("cornerRadius 1", self, self.maskToBounds)
-        }
-    }
-
     /**
      The corner curve of the view.
 
