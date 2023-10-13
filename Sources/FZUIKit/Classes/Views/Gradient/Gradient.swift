b639c06dc627e95375ca16eaac219ecb75dc9e4a
Florian Zand <florianzand@me.com>
2023-10-13 02:56:44 +0200

    Update Gradient.swift

--- Sources/FZUIKit/Classes/Views/Gradient/Gradient.swift
+++ Sources/FZUIKit/Classes/Views/Gradient/Gradient.swift
@@ -14,7 +14,7 @@ import AppKit
 import UIKit
 #endif
 
-public struct Gradient {
+public struct Gradient: Hashable {
     
     /// The array of color stops.
     public var stops: [Stop] = []
@@ -93,7 +93,7 @@ public struct Gradient {
 
 extension Gradient {
     /// The gradient type.
-    public enum GradientType: String {
+    public enum GradientType: String, Hashable {
         case linear = "axial"
         case conic = "conic"
         case radial = "radial"
@@ -108,7 +108,7 @@ extension Gradient {
     }
     
     /// One color stop in the gradient.
-    public struct Stop {
+    public struct Stop: Hashable {
         /// The color for the stop.
         public var color: NSUIColor
         /// The parametric location of the stop.
@@ -121,7 +121,7 @@ extension Gradient {
     }
     
     /// A point in the gradient.
-    public struct Point {
+    public struct Point: Hashable {
         public var x: CGFloat
         public var y: CGFloat
         
@@ -155,6 +155,87 @@ extension Gradient {
         public static var bottomLeading = Point(x: 0.0, y: 1.0)
         public static var bottom = Point(x: 0.5, y: 1.0)
         public static var bottomTrailing = Point(x: 1.0, y: 1.0)
+    }
+}
+
+extension Gradient.Stop: SpringInterpolatable, VelocityProviding {
+    public static func updateValue(spring: Spring, value: Gradient.Stop, target: Gradient.Stop, velocity: Gradient.Stop, dt: TimeInterval) -> (value: Gradient.Stop, velocity: Gradient.Stop) {
+        let color = NSUIColor.updateValue(spring: spring, value: value.color, target: target.color, velocity: velocity.color, dt: dt)
+        let location = CGFloat.updateValue(spring: spring, value: value.location, target: target.location, velocity: velocity.location, dt: dt)
+        return (Gradient.Stop(color: color.value, location: location.value), Gradient.Stop(color: color.velocity, location: velocity.location))
+    }
+    
+    public var scaledIntegral: Gradient.Stop {
+        Gradient.Stop(color: self.color.scaledIntegral, location: self.location.scaledIntegral)
+    }
+    
+    public static var zero: Gradient.Stop {
+        Gradient.Stop(color: .zero, location: .zero)
+    }
+}
+
+extension Gradient: SpringInterpolatable, VelocityProviding {
+    public static func updateValue(spring: Spring, value: Gradient, target: Gradient, velocity: Gradient, dt: TimeInterval) -> (value: Gradient, velocity: Gradient) {
+        var value = value
+        var target = target
+        var velocity = velocity
+        let diff = target.stops.count - value.stops.count
+        if diff < 0 {
+            let toRemove = (diff * -1)
+            let keep = (value.stops.count - toRemove)
+            for var stop in value.stops[keep..<value.stops.count] {
+                stop.color = stop.color.withAlphaComponent(0.0)
+                target.stops.append(stop)
+                if velocity.stops.count < target.stops.count {
+                    velocity.stops.append(.zero)
+                }
+            }
+        } else if diff > 0 {
+            let count = value.stops.count
+            for i in 0..<diff {
+                var targetStop = target.stops[count+i]
+                targetStop.color = targetStop.color.withAlphaComponent(0.0)
+                value.stops.append(targetStop)
+                if velocity.stops.count < value.stops.count {
+                    velocity.stops.append(.zero)
+                }
+            }
+        }
+        
+        Swift.print("stops", value.stops.count, target.stops.count, velocity.stops.count)
+        Swift.print("value colors")
+        for stop in value.stops {
+            Swift.print(stop.color)
+        }
+        Swift.print("target colors")
+        for stop in target.stops {
+            Swift.print(stop.color)
+        }
+        
+        var stops: [(value: Gradient.Stop, velocity: Gradient.Stop)] = []
+        for i in 0..<target.stops.count {
+            stops.append(Gradient.Stop.updateValue(spring: spring, value: value.stops[i], target: target.stops[i], velocity: velocity.stops[i], dt: dt))
+        }
+        
+        let valueStops = stops.compactMap({$0.value})
+        let velocityStops = stops.compactMap({$0.velocity})
+
+        let startPoint = CGPoint.updateValue(spring: spring, value: value.startPoint.point, target: target.startPoint.point, velocity: velocity.startPoint.point, dt: dt)
+        let endPoint = CGPoint.updateValue(spring: spring, value: value.endPoint.point, target: target.endPoint.point, velocity: velocity.endPoint.point, dt: dt)
+        
+        return (Gradient(stops: valueStops, startPoint: .init(startPoint.value), endPoint: .init(endPoint.value), type: target.type), Gradient(stops: velocityStops, startPoint: .init(startPoint.velocity), endPoint: .init(endPoint.velocity), type: target.type))
+    }
+    
+    public typealias ValueType = Gradient
+    
+    public typealias VelocityType = Gradient
+    
+    public var scaledIntegral: Gradient {
+        Gradient(stops: stops.compactMap({$0.scaledIntegral}), startPoint: .init(startPoint.point.scaledIntegral), endPoint: .init(endPoint.point.scaledIntegral), type: type)
+    }
+    
+    public static var zero: Gradient {
+        Gradient(stops: [])
     }
 }
 
