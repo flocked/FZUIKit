4633a60c9c2d2f3f583196b42dbe993db48564b3
Florian Zand <florianzand@me.com>
2023-10-18 18:45:27 +0200

    Update NSView+.swift

--- Sources/FZUIKit/Extensions/AppKit/View/NSView+.swift
+++ Sources/FZUIKit/Extensions/AppKit/View/NSView+.swift
@@ -508,9 +508,7 @@ extension NSView {
     
     @objc dynamic internal var dashedBorderWidth: CGFloat {
         get { self.dashedBorderLayer?.borderWidth ?? 0 }
-        set { 
-            Swift.print("dashedBorderWidth", newValue)
-            self.dashedBorderLayer?.borderWidth = newValue }
+        set {  self.dashedBorderLayer?.borderWidth = newValue }
     }
     
     @objc dynamic internal var dashedBorderInsetsTop: CGFloat {
@@ -949,7 +947,6 @@ internal extension NSView {
     }
     
     @objc func swizzled_Animation(forKey key: NSAnimatablePropertyKey) -> Any? {
-        Swift.print("swizzled_Animation", key)
         if NSViewAnimationKeys.contains(key) {
             let animation = CABasicAnimation()
             animation.timingFunction = .default
