b902f135d142a6f8b62d411b91d875b2e81cd3be
Florian Zand <florianzand@me.com>
2023-10-19 01:46:35 +0200

    Update NSView+.swift

--- Sources/FZUIKit/Extensions/AppKit/View/NSView+.swift
+++ Sources/FZUIKit/Extensions/AppKit/View/NSView+.swift
@@ -535,16 +535,18 @@ extension NSView {
                 layer?.shadowColor = (newValue?.withAlphaComponent(0.0) ?? .clear).cgColor
             }
             */
-            _shadowColor = newValue
+            shadowColorT = newValue
             savedShadowColor = newValue
         }
     }
     
-    @objc internal dynamic var _shadowColor: NSColor? {
+    @objc internal dynamic var shadowColorT: NSColor? {
         get { layer?.shadowColor?.nsColor }
         set {
+            Swift.print(newValue ?? "nil")
             self.wantsLayer = true
             layer?.shadowColor = newValue?.cgColor }
+
     }
     
     /**
@@ -873,7 +875,7 @@ internal extension NSView {
    }
 }
 
-private let NSViewAnimationKeys = ["transform", "transform3D", "anchorPoint", "_cornerRadius", "roundedCorners", "borderWidth", "borderColor", "mask", "_backgroundColor", "left", "right", "top", "bottom", "topLeft", "topCenter", "topRight", "centerLeft", "center", "centerRight", "bottomLeft", "bottomCenter", "bottomRight", "centerX", "centerY", "_shadowColor", "shadowOffset", "shadowOpacity", "shadowRadius", "shadowPath", "innerShadowColor", "innerShadowOffset", "innerShadowOpacity", "innerShadowRadius", "dashedBorderDashPattern0", "dashedBorderDashPattern1", "dashedBorderDashPattern2", "dashedBorderDashPattern3", "dashedBorderDashPattern4", "dashedBorderDashPattern5", "dashedBorderInsetsTop", "dashedBorderInsetsBottom", "dashedBorderInsetsLeading", "dashedBorderInsetsTrailing", "dashedBorderColor", "dashedBorderWidth"]
+private let NSViewAnimationKeys = ["transform", "transform3D", "anchorPoint", "_cornerRadius", "roundedCorners", "borderWidth", "borderColor", "mask", "_backgroundColor", "left", "right", "top", "bottom", "topLeft", "topCenter", "topRight", "centerLeft", "center", "centerRight", "bottomLeft", "bottomCenter", "bottomRight", "centerX", "centerY", "shadowColorT", "shadowOffset", "shadowOpacity", "shadowRadius", "shadowPath", "innerShadowColor", "innerShadowOffset", "innerShadowOpacity", "innerShadowRadius", "dashedBorderDashPattern0", "dashedBorderDashPattern1", "dashedBorderDashPattern2", "dashedBorderDashPattern3", "dashedBorderDashPattern4", "dashedBorderDashPattern5", "dashedBorderInsetsTop", "dashedBorderInsetsBottom", "dashedBorderInsetsLeading", "dashedBorderInsetsTrailing", "dashedBorderColor", "dashedBorderWidth"]
 #endif
 
 /*
