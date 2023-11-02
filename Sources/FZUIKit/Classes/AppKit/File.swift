45c7badf3f5e05c96ae384c6f4f72c225c9509f3
Florian Zand <florianzand@me.com>
2023-11-02 02:55:12 +0100

    updated

---
+++ Sources/FZUIKit/Classes/AppKit/File.swift
@@ -0,0 +1,124 @@
+//
+//  File.swift
+//  
+//
+//  Created by Florian Zand on 01.11.23.
+//
+
+import AppKit
+
+public class DockTile {
+    
+    
+    
+    internal var dockTile: NSDockTile {
+        NSApp.dockTile
+    }
+    
+    /// The badge label of the string.
+    public var badgeLabel: String? {
+        get { dockTile.badgeLabel }
+        set { dockTile.badgeLabel = newValue }
+    }
+    
+    /// Sets the badge label of the string.
+    @discardableResult
+    public func badgeLabel(_ label: String?) -> Self {
+        self.badgeLabel = label
+        return self
+    }
+    
+    public func display() {
+        if dockTile.contentView != nil {
+            dockTile.contentView = nil
+        }
+        dockTile.display()
+    }
+}
+
+extension DockTile {
+    public class BaseTile {
+        internal var dockTile: NSDockTile {
+            NSApp.dockTile
+        }
+        
+        /// The badge label of the string.
+        public var badgeLabel: String? {
+            get { dockTile.badgeLabel }
+            set { dockTile.badgeLabel = newValue }
+        }
+        
+        /// Sets the badge label of the string.
+        @discardableResult
+        public func badgeLabel(_ label: String?) -> Self {
+            self.badgeLabel = label
+            return self
+        }
+        
+        public func display() {
+            if dockTile.contentView != nil {
+                dockTile.contentView = nil
+            }
+            dockTile.display()
+        }
+    }
+}
+
+extension DockTile {
+    public class ContentConfiguration: BaseTile {
+        internal var view: ContentDisplayView
+        
+        public var configuration: NSContentConfiguration {
+            get { view.configuration }
+            set { view.configuration = newValue }
+        }
+        
+        @discardableResult
+        public func configuration(_ configuration: NSContentConfiguration) -> Self {
+            self.configuration = configuration
+            return self
+        }
+        
+        public init(_ configuration: NSContentConfiguration) {
+            view = ContentDisplayView(configuration: configuration)
+        }
+        
+        public override func display() {
+            if dockTile.contentView != view {
+                dockTile.contentView = view
+            }
+            dockTile.display()
+        }
+        
+        internal class ContentDisplayView: NSView {
+            /// The current configuration of the view.
+            public var configuration: NSContentConfiguration {
+                didSet {
+                    updateConfiguration()
+                }
+            }
+            
+            var contentView: (NSView & NSContentView)? = nil
+            
+            func updateConfiguration() {
+                if contentView?.supports(configuration) == true {
+                    contentView?.configuration = configuration
+                } else {
+                    contentView?.removeFromSuperview()
+                    contentView = configuration.makeContentView()
+                    self.addSubview(withConstraint: contentView!)
+                }
+            }
+            
+            init(configuration: NSContentConfiguration) {
+                self.configuration = configuration
+                super.init(frame: .zero)
+                updateConfiguration()
+            }
+            
+            required init?(coder: NSCoder) {
+                fatalError("init(coder:) has not been implemented")
+            }
+        }
+    }
+}
---
+++ Sources/FZUIKit/Extensions/AppKit/NSPopover+.swift
@@ -0,0 +1,126 @@
+//
+//  NSPopover+.swift
+//
+//
+//  Created by Florian Zand on 02.11.23.
+//
+
+#if os(macOS)
+import AppKit
+import FZSwiftUtils
+
+extension NSPopover {
+    /// Creates and returns a popover with the specified view.
+    public convenience init(view: NSView) {
+        self.init()
+        let viewController = NSViewController()
+        viewController.view = view
+        self.contentViewController = viewController
+        self.contentSize = view.bounds.size
+    }
+    
+    /// Detaches the popover and optionally hides the close button.
+    public func detach(hideCloseButton: Bool = false) {
+        if self.isDetached == false {
+            let detach = NSSelectorFromString("detach")
+            if self.responds(to: detach) {
+                self.perform(detach)
+            }
+        }
+        closeButton?.isHidden = hideCloseButton
+    }
+    
+    /**
+     Shows the popover anchored to the specified view.
+     
+     - Parameters:
+        - positioningRect: The rectangle within positioningView relative to which the popover should be positioned. Normally set to the bounds of positioningView. May be an empty rectangle, which will default to the bounds of positioningView.
+        - positioningView: The view relative to which the popover should be positioned. Causes the method to raise invalidArgumentException if nil.
+        - preferredEdge: The edge of positioningView the popover should prefer to be anchored to.
+        - trackViewFrame: A Boolean value that indicates whether to automatically reposition the popover when the view frame changes.
+     */
+    public func show(relativeTo positioningRect: CGRect, of positioningView: NSView, preferredEdge: NSRectEdge, trackViewFrame: Bool) {
+        self.show(relativeTo: positioningRect, of: positioningView, preferredEdge: preferredEdge, hideArrow: false, trackViewFrame: trackViewFrame)
+    }
+    
+    /**
+     Shows the popover anchored to the specified view.
+     
+     - Parameters:
+        - positioningRect: The rectangle within positioningView relative to which the popover should be positioned. Normally set to the bounds of positioningView. May be an empty rectangle, which will default to the bounds of positioningView.
+        - positioningView: The view relative to which the popover should be positioned. Causes the method to raise invalidArgumentException if nil.
+        - preferredEdge: The edge of positioningView the popover should prefer to be anchored to.
+        - hideArrow: A Boolean value that indicates whether to hide the arrow of the popover.
+        - trackViewFrame: A Boolean value that indicates whether to automatically reposition the popover when the view frame changes.
+     */
+    public func show(relativeTo positioningRect: CGRect, of positioningView: NSView, preferredEdge: NSRectEdge, hideArrow: Bool, trackViewFrame: Bool = true) {
+        isOpeningPopover = true
+        self.dismissNoArrow()
+        if hideArrow == false {
+            self.show(relativeTo: positioningRect, of: positioningView, preferredEdge: preferredEdge)
+        } else {
+            let noArrowView = NSView(frame: positioningView.frame)
+            switch preferredEdge {
+            case .minX:
+                noArrowView.frame.origin.x += 10
+            case .maxX:
+                noArrowView.frame.origin.x -= 10
+            case .minY:
+                noArrowView.frame.origin.y += 10
+            case .maxY:
+                noArrowView.frame.origin.y -= 10
+            default: break
+            }
+            self.noArrowView = noArrowView
+            positioningView.superview?.addSubview(noArrowView, positioned: .below,relativeTo: positioningView)
+            self.show(relativeTo: positioningRect, of: noArrowView, preferredEdge: preferredEdge)
+            noArrowView.frame =  NSMakeRect(0, -200, 10, 10)
+            willCloseObserver = NotificationCenter.default.observe(name: NSPopover.willCloseNotification, object: self, using: { notification in
+                (notification.object as? NSPopover)?.dismissNoArrow()
+            })
+        }
+        if trackViewFrame {
+            positioningViewFrameObserver = positioningView.observeChanges(for: \.frame, handler: { [weak self] old, new in
+                guard let self = self, old != new else { return }
+                if self.isOpeningPopover == false, self.isDetached == false, self.isShown == true {
+                    let animates = self.animates
+                    self.animates = false
+                    self.show(relativeTo: positioningRect, of: positioningView, preferredEdge: preferredEdge, hideArrow: hideArrow)
+                    self.animates = animates
+                }
+            })
+        }
+        isOpeningPopover = false
+    }
+    
+    internal func dismissNoArrow() {
+        self.noArrowView?.removeFromSuperview()
+        self.noArrowView = nil
+        self.willCloseObserver = nil
+    }
+    
+    internal var closeButton: NSButton? {
+        self.contentViewController?.view.superview?.subviews.last as? NSButton
+    }
+    
+    internal var willCloseObserver: NotificationToken? {
+        get { getAssociatedValue(key: "willClosePopoverObserver", object: self, initialValue: nil) }
+        set { set(associatedValue: newValue, key: "willClosePopoverObserver", object: self) }
+    }
+    
+    internal var noArrowView: NSView? {
+        get { getAssociatedValue(key: "noArrowView", object: self, initialValue: nil) }
+        set { set(associatedValue: newValue, key: "noArrowView", object: self) }
+    }
+    
+    internal var positioningViewFrameObserver: NSKeyValueObservation?  {
+        get { getAssociatedValue(key: "positioningFrameObserver", object: self, initialValue: nil) }
+        set { set(associatedValue: newValue, key: "positioningFrameObserver", object: self) }
+    }
+    
+    internal var isOpeningPopover: Bool  {
+        get { getAssociatedValue(key: "isOpeningPopover", object: self, initialValue: true) }
+        set { set(associatedValue: newValue, key: "isOpeningPopover", object: self) }
+    }
+}
+#endif
