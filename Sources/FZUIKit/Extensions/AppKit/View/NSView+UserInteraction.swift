35798fb3603724e55a1f9d63c125f514d0270136
Florian Zand <florianzand@me.com>
2023-11-09 10:54:29 +0100

    Update NSView+UserInteraction.swift

---
+++ Sources/FZUIKit/Extensions/AppKit/View/NSView+UserInteraction.swift
@@ -0,0 +1,240 @@
+//
+//  NSView+UserInteraction.swift
+//
+//
+//  Created by Florian Zand on 09.11.23.
+//
+
+#if os(macOS)
+
+import AppKit
+import FZSwiftUtils
+
+public extension NSView {
+    /**
+     A Boolean value that determines whether user events are ignored and removed from the event queue.
+     
+     When set to false`, mouse and keyboard events intended for the view are ignored and removed from the event queue. When set to `true`, events are delivered to the view normally. The default value of this property is `true`.
+     */
+    var isUserInteractionEnabled: Bool {
+        get { getAssociatedValue(key: "isUserInteractionEnabled", object: self, initialValue: true) }
+        set {
+            guard newValue != isUserInteractionEnabled else { return }
+            set(associatedValue: newValue, key: "isUserInteractionEnabled", object: self)
+            swizzleUserInteraction()
+            /*
+            if newValue == false {
+                UserInteractionMonitor.shared.addView(self)
+                if self.window?.firstResponder == self {
+                    self.resignFirstResponder()
+                }
+            } else {
+                UserInteractionMonitor.shared.removeView(self)
+            }
+             */
+        }
+    }
+    
+     func swizzleUserInteraction() {
+        guard didSwizzleUserInteraction == false else { return }
+        didSwizzleUserInteraction = true
+         Swift.print("swizzleUserInteraction")
+
+         do {
+             try self.replaceMethod(
+                 #selector(NSView.mouseDown(with:)),
+                 methodSignature: (@convention(c)  (AnyObject, Selector, NSEvent) -> ()).self,
+                 hookSignature: (@convention(block)  (AnyObject, NSEvent) -> ()).self) { store in { object, event in
+                     let view = (object as! NSView)
+                     guard view.isUserInteractionEnabled else { return }
+                     store.original(object, #selector(NSView.mouseDown(with:)), event)
+                 }
+                 }
+             try self.replaceMethod(
+                 #selector(NSView.mouseUp(with:)),
+                 methodSignature: (@convention(c)  (AnyObject, Selector, NSEvent) -> ()).self,
+                 hookSignature: (@convention(block)  (AnyObject, NSEvent) -> ()).self) { store in { object, event in
+                     let view = (object as! NSView)
+                     guard view.isUserInteractionEnabled else { return }
+                     store.original(object, #selector(NSView.mouseUp(with:)), event)
+                 }
+                 }
+             try self.replaceMethod(
+                 #selector(NSView.mouseDragged(with:)),
+                 methodSignature: (@convention(c)  (AnyObject, Selector, NSEvent) -> ()).self,
+                 hookSignature: (@convention(block)  (AnyObject, NSEvent) -> ()).self) { store in { object, event in
+                     let view = (object as! NSView)
+                     guard view.isUserInteractionEnabled else { return }
+                     store.original(object, #selector(NSView.mouseDragged(with:)), event)
+                 }
+                 }
+             try self.replaceMethod(
+                 #selector(NSView.rightMouseDown(with:)),
+                 methodSignature: (@convention(c)  (AnyObject, Selector, NSEvent) -> ()).self,
+                 hookSignature: (@convention(block)  (AnyObject, NSEvent) -> ()).self) { store in { object, event in
+                     let view = (object as! NSView)
+                     guard view.isUserInteractionEnabled else { return }
+                     store.original(object, #selector(NSView.rightMouseDown(with:)), event)
+                 }
+                 }
+             try self.replaceMethod(
+                 #selector(NSView.rightMouseUp(with:)),
+                 methodSignature: (@convention(c)  (AnyObject, Selector, NSEvent) -> ()).self,
+                 hookSignature: (@convention(block)  (AnyObject, NSEvent) -> ()).self) { store in { object, event in
+                     let view = (object as! NSView)
+                     guard view.isUserInteractionEnabled else { return }
+                     store.original(object, #selector(NSView.rightMouseUp(with:)), event)
+                 }
+                 }
+             try self.replaceMethod(
+                 #selector(NSView.rightMouseDragged(with:)),
+                 methodSignature: (@convention(c)  (AnyObject, Selector, NSEvent) -> ()).self,
+                 hookSignature: (@convention(block)  (AnyObject, NSEvent) -> ()).self) { store in { object, event in
+                     let view = (object as! NSView)
+                     guard view.isUserInteractionEnabled else { return }
+                     store.original(object, #selector(NSView.rightMouseDragged(with:)), event)
+                 }
+                 }
+             try self.replaceMethod(
+                 #selector(NSView.keyDown(with:)),
+                 methodSignature: (@convention(c)  (AnyObject, Selector, NSEvent) -> ()).self,
+                 hookSignature: (@convention(block)  (AnyObject, NSEvent) -> ()).self) { store in { object, event in
+                     let view = (object as! NSView)
+                     guard view.isUserInteractionEnabled else { return }
+                     store.original(object, #selector(NSView.keyDown(with:)), event)
+                 }
+                 }
+             try self.replaceMethod(
+                 #selector(NSView.keyUp(with:)),
+                 methodSignature: (@convention(c)  (AnyObject, Selector, NSEvent) -> ()).self,
+                 hookSignature: (@convention(block)  (AnyObject, NSEvent) -> ()).self) { store in { object, event in
+                     let view = (object as! NSView)
+                     guard view.isUserInteractionEnabled else { return }
+                     store.original(object, #selector(NSView.keyUp(with:)), event)
+                 }
+                 }
+             try self.replaceMethod(
+                 #selector(NSView.flagsChanged(with:)),
+                 methodSignature: (@convention(c)  (AnyObject, Selector, NSEvent) -> ()).self,
+                 hookSignature: (@convention(block)  (AnyObject, NSEvent) -> ()).self) { store in { object, event in
+                     let view = (object as! NSView)
+                     guard view.isUserInteractionEnabled else { return }
+                     store.original(object, #selector(NSView.flagsChanged(with:)), event)
+                 }
+                 }
+             try self.replaceMethod(
+                 #selector(NSView.magnify(with:)),
+                 methodSignature: (@convention(c)  (AnyObject, Selector, NSEvent) -> ()).self,
+                 hookSignature: (@convention(block)  (AnyObject, NSEvent) -> ()).self) { store in { object, event in
+                     let view = (object as! NSView)
+                     guard view.isUserInteractionEnabled else { return }
+                     store.original(object, #selector(NSView.magnify(with:)), event)
+                 }
+                 }
+             try self.replaceMethod(
+                 #selector(NSView.scrollWheel(with:)),
+                 methodSignature: (@convention(c)  (AnyObject, Selector, NSEvent) -> ()).self,
+                 hookSignature: (@convention(block)  (AnyObject, NSEvent) -> ()).self) { store in { object, event in
+                     let view = (object as! NSView)
+                     guard view.isUserInteractionEnabled else { return }
+                     store.original(object, #selector(NSView.scrollWheel(with:)), event)
+                 }
+                 }
+             try self.replaceMethod(
+                 #selector(NSView.mouseMoved(with:)),
+                 methodSignature: (@convention(c)  (AnyObject, Selector, NSEvent) -> ()).self,
+                 hookSignature: (@convention(block)  (AnyObject, NSEvent) -> ()).self) { store in { object, event in
+                     let view = (object as! NSView)
+                     guard view.isUserInteractionEnabled else { return }
+                     store.original(object, #selector(NSView.mouseMoved(with:)), event)
+                 }
+                 }
+             try self.replaceMethod(
+                 #selector(NSView.mouseEntered(with:)),
+                 methodSignature: (@convention(c)  (AnyObject, Selector, NSEvent) -> ()).self,
+                 hookSignature: (@convention(block)  (AnyObject, NSEvent) -> ()).self) { store in { object, event in
+                     let view = (object as! NSView)
+                     guard view.isUserInteractionEnabled else { return }
+                     store.original(object, #selector(NSView.mouseEntered(with:)), event)
+                 }
+                 }
+             try self.replaceMethod(
+                 #selector(NSView.mouseExited(with:)),
+                 methodSignature: (@convention(c)  (AnyObject, Selector, NSEvent) -> ()).self,
+                 hookSignature: (@convention(block)  (AnyObject, NSEvent) -> ()).self) { store in { object, event in
+                     let view = (object as! NSView)
+                     guard view.isUserInteractionEnabled else { return }
+                     store.original(object, #selector(NSView.mouseExited(with:)), event)
+                 }
+                 }
+         } catch {
+             Swift.print(error)
+         }
+    }
+    
+    var didSwizzleUserInteraction: Bool {
+        get { getAssociatedValue(key: "didSwizzleUserInteraction", object: self, initialValue: false) }
+        set { set(associatedValue: newValue, key: "didSwizzleUserInteraction", object: self) }
+    }
+}
+
+internal class UserInteractionMonitor {
+    static let shared = UserInteractionMonitor()
+    
+    var views: [Weak<NSView>] = []
+    
+    func addView(_ view: NSUIView) {
+        guard allViews.contains(view) == false else { return }
+        views.append(Weak(view))
+        setupMonitor()
+    }
+    
+    func removeView(_ view: NSUIView) {
+        if let index = views.firstIndex(where: { $0.object == view }) {
+            views.remove(at: index)
+            setupMonitor()
+        }
+    }
+    
+    var eventMonitor: NSEvent.Monitor? = nil
+        
+    func setupMonitor() {
+        Swift.print("setupMonitor")
+        if allViews.isEmpty == false {
+            if eventMonitor == nil {
+                eventMonitor = NSEvent.Monitor.local(for: .allUserInteractions) { event in
+                    
+                    if let firstResponder = NSApp.keyWindow?.firstResponder as? NSUIView {
+                        for view in self.allViews {
+                            if firstResponder == view || firstResponder.isDescendant(of: view) {
+                                return nil
+                            }
+                        }
+                    }
+                    
+                    if NSEvent.EventType.mouse.contains(event.type) {
+                        if let contentView = NSApp.keyWindow?.contentView {
+                            let mousePoint = event.location(in: contentView)
+                            if let hitView = contentView.hitTest(mousePoint) {
+                                for view in self.allViews {
+                                    if hitView == view || hitView.isDescendant(of: view) {
+                                        return nil
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    return event
+                }
+            }
+        } else {
+            eventMonitor = nil
+        }
+    }
+    
+    var allViews: [NSUIView] {
+        views.compactMap({$0.object})
+    }
+}
+
+#endif
