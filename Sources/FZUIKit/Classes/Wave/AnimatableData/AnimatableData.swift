d2c06d30a37e6a0883c750cafaf91a4cc9dd79ba
Florian Zand <florianzand@me.com>
2023-11-10 01:30:31 +0100

    Update AnimatableData.swift

--- Sources/FZUIKit/Classes/Wave/AnimatableData/AnimatableData.swift
+++ Sources/FZUIKit/Classes/Wave/AnimatableData/AnimatableData.swift
@@ -149,13 +149,16 @@ extension NSUIColor: AnimatableData {
 
 extension AnimatableData where Self: CGColor {
     public init(_ animatableData: AnimatableVector) {
-        self = NSUIColor(animatableData).cgColor as! Self
+        self.init(red: animatableData[0], green: animatableData[1], blue: animatableData[2], alpha: animatableData[3])
+       // self = NSUIColor(animatableData).cgColor as! Self
     }
 }
 
 extension CGColor: AnimatableData {
     public var animatableData: AnimatableVector {
-        let rgba = self.nsUIColor?.rgbaComponents() ?? (red: 0, green: 0, blue: 0, alpha: 0)
+        let rgba = self.rgbaComponents() ?? (red: 0, green: 0, blue: 0, alpha: 0)
+        
+      //  let rgba = self.nsUIColor?.rgbaComponents() ?? (red: 0, green: 0, blue: 0, alpha: 0)
         return [rgba.red, rgba.green, rgba.blue, rgba.alpha]
     }
     
