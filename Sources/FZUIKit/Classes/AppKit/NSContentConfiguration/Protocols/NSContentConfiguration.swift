366d097ee432f2f3f15628bd379f743c4d264647
Florian Zand <florianzand@me.com>
2023-12-22 18:00:15 +0100

    updated

--- Sources/FZUIKit/Classes/AppKit/NSContentConfiguration/Protocols/NSContentConfiguration.swift
+++ Sources/FZUIKit/Classes/AppKit/NSContentConfiguration/Protocols/NSContentConfiguration.swift
@@ -14,14 +14,11 @@ import AppKit
  This protocol provides a blueprint for a content configuration object, which encompasses default styling and content for a content view. The content configuration encapsulates all of the supported properties and behaviors for content view customization. You use the configuration to create the content view.
  */
 public protocol NSContentConfiguration {
-    /**
-     Creates a new instance of the content view using this configuration.
-     */
+    
+    /// Creates a new instance of the content view using this configuration.
     func makeContentView() -> NSView & NSContentView
     
-    /**
-     Generates a configuration for the specified state by applying the configuration’s default values for that state to any properties that you haven’t customized.
-     */
+    /// Generates a configuration for the specified state by applying the configuration’s default values for that state to any properties that you haven’t customized.
     func updated(for state: NSConfigurationState) -> Self
 }
 
