3da4858d560c9d6c52557ca2a10094a8e1573a8b
Florian Zand <florianzand@me.com>
2023-12-22 04:07:49 +0100

    Update NSView+BackgroundStyle.swift

--- Sources/FZUIKit/Extensions/View/AppKit/NSView+BackgroundStyle.swift
+++ Sources/FZUIKit/Extensions/View/AppKit/NSView+BackgroundStyle.swift
@@ -51,7 +51,11 @@ extension NSView {
      */
     @objc open dynamic func setBackgroundStyle(_ backgroundStyle: NSView.BackgroundStyle) {
         let text = (self as? NSTextField)?.stringValue ?? ""
-        Swift.debugPrint("setBackgroundStyle \(backgroundStyle.rawValue), \(depthOfView) \(type(of: self)), ", self.superview != nil ? "\(type(of: superview!)), " : "", text)
+        if let superview = self.superview {
+            Swift.print(depthOfView, "setBackgroundStyle:", backgroundStyle.rawValue, text, type(of: self), type(of: superview))
+        } else {
+            Swift.debugPrint(depthOfView, "setBackgroundStyle:", backgroundStyle.rawValue, text, type(of: self), self.superview != nil ? type(of: superview!) : "")
+        }
         
         (self as? ViewBackgroundStyleCustomizable)?.backgroundStyle = backgroundStyle
         
