010a2aed3b8892ec706c75c4d0b09083820e58f3
Florian Zand <florianzand@me.com>
2023-12-28 02:50:18 +0100

    Create NSCollectionViewItem+.swift

---
+++ Sources/FZUIKit/Extensions/View/AppKit/NSCollectionView/NSCollectionViewItem+.swift
@@ -0,0 +1,32 @@
+//
+//  NSCollectionViewItem+.swift
+//
+//
+//  Created by Florian Zand on 28.12.23.
+//
+
+#if os(macOS)
+
+import AppKit
+
+extension NSCollectionViewItem {
+    /// The previous item in the collection view, or `nil` if there isn't a previous item or the item isn't in a collection view..
+    @objc open var previousItem: NSCollectionViewItem? {
+        if let indexPath = self.collectionView?.indexPath(for: self), indexPath.item - 1 >= 0 {
+            let previousIndexPath = IndexPath(item: indexPath.item - 1, section: indexPath.section)
+            return self.collectionView?.item(at: previousIndexPath)
+        }
+        return nil
+    }
+    
+    /// The next item in the collection view, or `nil` if there isn't a next item or the item isn't in a collection view..
+    @objc open var nextItem: NSCollectionViewItem? {
+        if let indexPath = self.collectionView?.indexPath(for: self), indexPath.item + 1 < (self.collectionView?.numberOfItems(inSection: indexPath.section) ?? -10) {
+            let nextIndexPath = IndexPath(item: indexPath.item + 1, section: indexPath.section)
+            return self.collectionView?.item(at: nextIndexPath)
+        }
+        return nil
+    }
+}
+
+#endif
