e6e944386d9e1ccb66518ee7bf2e022e07706c34
Florian Zand <florianzand@me.com>
2024-01-17 20:46:39 +0100

    Update NSProgressIndicator+.swift

--- Sources/FZUIKit/Extensions/View/AppKit/NSProgressIndicator+.swift
+++ Sources/FZUIKit/Extensions/View/AppKit/NSProgressIndicator+.swift
@@ -13,9 +13,30 @@ extension NSProgressIndicator {
     public static var spinning: NSProgressIndicator {
         let progressIndicator = NSProgressIndicator()
         progressIndicator.style = .spinning
-        progressIndicator.stopAnimation(nil)
+        progressIndicator.startAnimation(nil)
         progressIndicator.sizeToFit()
         return progressIndicator
+    }
+    
+    /**
+     Creates a bar progress indicator.
+     
+     - Parameters:
+        - minValue: The minimum value. The default value is `0`.
+        - maxValue: The maximum value.  The default value is `100.0`.
+        - value: The value.  The default value is `0`.
+        - width: The width of the progress indicator. The default value is `200.0`.
+     */
+    public static func bar(minValue: Double = 0.0, maxValue: Double = 0.0, value: Double = 0.0, width: CGFloat = 200) -> NSProgressIndicator {
+        let progressIndicator = NSProgressIndicator()
+        progressIndicator.minValue = minValue
+        progressIndicator.maxValue = maxValue
+        progressIndicator.doubleValue = value
+        progressIndicator.isIndeterminate = false
+        progressIndicator.sizeToFit()
+        progressIndicator.frame.size.width = width
+        return progressIndicator
+
     }
 }
 
