8903e2053a70fedf8cab1e078c0eea39db241ce1
Florian Zand <florianzand@me.com>
2024-01-18 06:51:52 +0100

    Update NSCursor+.swift

--- Sources/FZUIKit/Extensions/AppKit/Misc/NSCursor+.swift
+++ Sources/FZUIKit/Extensions/AppKit/Misc/NSCursor+.swift
@@ -41,11 +41,12 @@ import Combine
 
          - Parameters:
             - image: The animated image (e.g. a GIF) to assign to the cursor.
+            - maximumSize: The maximum size of the animated image. The default value is `zero`. A width or height of `0` means that the system doesn’t constrain the size for that dimension. If the image exceeds this size on either dimension, the system reduces the size proportionately, maintaining the aspect ratio.
             - frameDuration: The duration each image is displayed. The default value is `nil` which uses the frame duration of the animated image.
             - hotSpot: The point to set as the cursor's hot spot. The default value is `zero`.
          */
-        public static func animated(_ image: NSImage, frameDuration: TimeInterval? = nil, hotSpot: CGPoint = .zero) -> NSCursor {
-            return NSCursor(animated: image, frameDuration: frameDuration, hotSpot: hotSpot)
+        public static func animated(_ image: NSImage, maximumSize: CGSize = .zero, frameDuration: TimeInterval? = nil, hotSpot: CGPoint = .zero) -> NSCursor {
+            return NSCursor(animated: image, maximumSize: maximumSize, frameDuration: frameDuration, hotSpot: hotSpot)
         }
         
         /**
@@ -53,11 +54,12 @@ import Combine
 
          - Parameters:
             - images: The images to assign to the cursor.
+            - maximumSize: The maximum size of the animated images. The default value is `zero`. A width or height of `0` means that the system doesn’t constrain the size for that dimension. If the images exceeds this size on either dimension, the system reduces the size proportionately, maintaining the aspect ratio.
             - frameDuration: The duration each image is displayed.
             - hotSpot: The point to set as the cursor's hot spot. The default value is `zero`.
          */
-        public static func animated(_ images: [NSImage], frameDuration: TimeInterval, hotSpot: CGPoint = .zero) -> NSCursor {
-            return NSCursor(animated: images, frameDuration: frameDuration, hotSpot: hotSpot)
+        public static func animated(_ images: [NSImage], maximumSize: CGSize = .zero, frameDuration: TimeInterval, hotSpot: CGPoint = .zero) -> NSCursor {
+            return NSCursor(animated: images, maximumSize: maximumSize, frameDuration: frameDuration, hotSpot: hotSpot)
         }
 
         /**
@@ -65,12 +67,23 @@ import Combine
 
          - Parameters:
             - image: The animated image (e.g. a GIF) to assign to the cursor.
+            - maximumSize: The maximum size of the animated image. The default value is `zero`. A width or height of `0` means that the system doesn’t constrain the size for that dimension. If the image exceeds this size on either dimension, the system reduces the size proportionately, maintaining the aspect ratio.
             - frameDuration: The duration each image is displayed. The default value is `nil` which uses the frame duration of the animated image.
             - hotSpot: The point to set as the cursor's hot spot. The default value is `zero`.
          */
-        public convenience init(animated image: NSImage, frameDuration: TimeInterval? = nil, hotSpot: CGPoint = .zero) {
+        public convenience init(animated image: NSImage, maximumSize: CGSize = .zero, frameDuration: TimeInterval? = nil, hotSpot: CGPoint = .zero) {
             if image.isAnimated, let imageFrames = try? image.frames?.collect() {
-                self.init(animated: imageFrames.compactMap({ImageFrame($0.image.nsImage, $0.duration ?? 0.12)}), hotSpot: hotSpot)
+                self.init(animated: imageFrames.compactMap({
+                    if maximumSize.width > 0, maximumSize.height > 0 {
+                        return ImageFrame($0.image.nsImage.resized(toFit: maximumSize), $0.duration ?? 0.12)
+                    } else if maximumSize.width > 0 {
+                        return ImageFrame($0.image.nsImage.resized(toWidth: maximumSize.width), $0.duration ?? 0.12)
+                    } else if maximumSize.height > 0 {
+                        return ImageFrame($0.image.nsImage.resized(toHeight: maximumSize.height), $0.duration ?? 0.12)
+                    } else {
+                        return ImageFrame($0.image.nsImage, $0.duration ?? 0.12)
+                    }
+                }), hotSpot: hotSpot)
             } else {
                 self.init(image: image, hotSpot: hotSpot)
             }
@@ -81,10 +94,19 @@ import Combine
 
          - Parameters:
             - images: The images to assign to the cursor.
+            - maximumSize: The maximum size of the animated images. The default value is `zero`. A width or height of `0` means that the system doesn’t constrain the size for that dimension. If the images exceeds this size on either dimension, the system reduces the size proportionately, maintaining the aspect ratio.
             - frameDuration: The duration each image is displayed.
             - hotSpot: The point to set as the cursor's hot spot. The default value is `zero`.
          */
-        public convenience init(animated images: [NSImage], frameDuration: TimeInterval, hotSpot: CGPoint = .zero) {
+        public convenience init(animated images: [NSImage], maximumSize: CGSize = .zero, frameDuration: TimeInterval, hotSpot: CGPoint = .zero) {
+            var images = images
+            if maximumSize.width > 0, maximumSize.height > 0 {
+                images = images.compactMap({$0.resized(toFit: maximumSize)})
+            } else if maximumSize.width > 0 {
+                images = images.compactMap({$0.resized(toWidth: maximumSize.width)})
+            } else if maximumSize.height > 0 {
+                images = images.compactMap({$0.resized(toHeight: maximumSize.height)})
+            }
             self.init(animated: images.compactMap({ImageFrame($0, frameDuration)}), hotSpot: hotSpot)
         }
         
