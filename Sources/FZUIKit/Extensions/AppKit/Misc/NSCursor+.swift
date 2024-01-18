923e3a61dbeb5a7fcd6caef6019e94623130fc04
Florian Zand <florianzand@me.com>
2024-01-18 06:09:06 +0100

    Update NSCursor+.swift

--- Sources/FZUIKit/Extensions/AppKit/Misc/NSCursor+.swift
+++ Sources/FZUIKit/Extensions/AppKit/Misc/NSCursor+.swift
@@ -9,14 +9,15 @@
     import AppKit
     import Foundation
     import FZSwiftUtils
+import Combine
 
-    public extension NSCursor {
+    extension NSCursor {
         /**
          Returns the resize-diagonal system cursor (from north-west to south-east).
          
          Use this cursor for resizing of a bottom right or top left corner.
          */
-        static var resizeDiagonal: NSCursor? {
+        public static var resizeDiagonal: NSCursor? {
             if let url = Bundle.module.url(forResource: "resizenorthwestsoutheast", withExtension: "pdf"), let image = NSImage(contentsOf: url) {
                 return NSCursor(image: image, hotSpot: NSCursor.arrow.hotSpot)
             }
@@ -28,11 +29,35 @@
          
          Use this cursor for resizing of a bottom left or top right corner.
          */
-        static var resizeDiagonalAlt: NSCursor? {
+        public static var resizeDiagonalAlt: NSCursor? {
             if let url = Bundle.module.url(forResource: "resizenortheastsouthwest", withExtension: "pdf"), let image = NSImage(contentsOf: url) {
                 return NSCursor(image: image, hotSpot: NSCursor.arrow.hotSpot)
             }
             return nil
+        }
+        
+        /**
+         Returns an animated cursor for the specified animated image.
+
+         - Parameters:
+            - image: The animated image (e.g. a GIF) to assign to the cursor.
+            - frameDuration: The duration each image is displayed. The default value is `nil` which uses the frame duration of the animated image.
+            - hotSpot: The point to set as the cursor's hot spot. The default value is `zero`.
+         */
+        public static func animated(_ image: NSImage, frameDuration: TimeInterval? = nil, hotSpot: CGPoint = .zero) -> NSCursor {
+            return NSCursor(animated: image, frameDuration: frameDuration, hotSpot: hotSpot)
+        }
+        
+        /**
+         Returns an animated cursor for the specified images.
+
+         - Parameters:
+            - images: The images to assign to the cursor.
+            - frameDuration: The duration each image is displayed.
+            - hotSpot: The point to set as the cursor's hot spot. The default value is `zero`.
+         */
+        public static func animated(_ images: [NSImage], frameDuration: TimeInterval, hotSpot: CGPoint = .zero) -> NSCursor {
+            return NSCursor(animated: images, frameDuration: frameDuration, hotSpot: hotSpot)
         }
 
         /**
@@ -40,17 +65,18 @@
 
          - Parameters:
             - image: The animated image (e.g. a GIF) to assign to the cursor.
-            - frameDuration: The duration each image is displayed.
-            - hotSpot: The point to set as the cursor's hot spot.
+            - frameDuration: The duration each image is displayed. The default value is `nil` which uses the frame duration of the animated image.
+            - hotSpot: The point to set as the cursor's hot spot. The default value is `zero`.
          */
-        convenience init(animated image: NSImage, frameDuration: TimeInterval? = nil, hotSpot: CGPoint) {
+        public convenience init(animated image: NSImage, frameDuration: TimeInterval? = nil, hotSpot: CGPoint = .zero) {
             if image.isAnimated, let imageFrames = try? image.frames?.collect() {
-                var frameDuration = frameDuration ?? imageFrames.compactMap(\.duration).average()
-                if frameDuration == 0.0 {
-                    frameDuration = 0.12
+                var frameDurations: [TimeInterval] = []
+                var images: [NSImage] = []
+                for imageFrame in imageFrames {
+                    frameDurations.append(imageFrame.duration ?? 0.12)
+                    images.append(imageFrame.image.nsImage)
                 }
-                let images = imageFrames.compactMap(\.image.nsImage)
-                self.init(animated: images, frameDuration: frameDuration, hotSpot: hotSpot)
+                self.init(animated: images, frameDurations: frameDurations, hotSpot: hotSpot)
             } else {
                 self.init(image: image, hotSpot: hotSpot)
             }
@@ -62,9 +88,13 @@
          - Parameters:
             - images: The images to assign to the cursor.
             - frameDuration: The duration each image is displayed.
-            - hotSpot: The point to set as the cursor's hot spot.
+            - hotSpot: The point to set as the cursor's hot spot. The default value is `zero`.
          */
-        convenience init(animated images: [NSImage], frameDuration: TimeInterval, hotSpot: CGPoint) {
+        public convenience init(animated images: [NSImage], frameDuration: TimeInterval, hotSpot: CGPoint = .zero) {
+            self.init(animated: images, frameDurations: Array(repeating: frameDuration, count: images.count), hotSpot: hotSpot)
+        }
+        
+        convenience init(animated images: [NSImage], frameDurations: [TimeInterval], hotSpot: CGPoint = .zero) {
             self.init(image: images.first ?? NSCursor.current.image, hotSpot: images.isEmpty ? NSCursor.current.hotSpot : hotSpot)
             do {
                 try replaceMethod(
@@ -73,7 +103,7 @@
                     hookSignature: (@convention(block) (AnyObject) -> Void).self
                 ) { store in { object in
                     store.original(object, #selector(NSCursor.set))
-                    NSCursorAnimator.shared.frameDuration = frameDuration
+                    NSCursorAnimator.shared.frameDurations = frameDurations
                     NSCursorAnimator.shared.hotSpot = hotSpot
                     NSCursorAnimator.shared.images = images
                     NSCursorAnimator.shared.restart()
@@ -89,6 +119,7 @@
             var timer: Timer?
             var images: [NSImage] = []
             var frameDuration: TimeInterval = 0.0
+            var frameDurations: [TimeInterval] = []
             var index: Int = 0
             var hotSpot: CGPoint = .init(x: 8, y: 8)
 
@@ -97,20 +128,44 @@
                 start()
             }
 
+            var displayLink: AnyCancellable? = nil
+            var lastFrameTime = CFAbsoluteTimeGetCurrent()
+ 
+            
             func start() {
                 guard frameDuration != 0.0, images.count > 1 else {
                     stop()
                     return
                 }
+                guard displayLink == nil else { return }
+                lastFrameTime = CFAbsoluteTimeGetCurrent()
+                displayLink = DisplayLink.shared.sink { [weak self] frame in
+                    guard let self = self else { return }
+                    let current = CFAbsoluteTimeGetCurrent()
+                    if current - self.lastFrameTime > self.frameDurations[self.index] {
+                        self.lastFrameTime = current
+                        self.advanceImage()
+                    }
+
+                }
+                
+                /*
                 timer?.invalidate()
                 timer = Timer.scheduledTimer(withTimeInterval: frameDuration, repeats: true, block: { _ in
                     self.advanceImage()
                 })
+                 */
+                
             }
 
             func stop() {
+                displayLink?.cancel()
+                displayLink = nil
+                
+                /*
                 timer?.invalidate()
                 timer = nil
+                 */
                 index = 0
             }
 
