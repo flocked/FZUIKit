47478d11e9bbc253210ad7b058585b878d560a1b
Florian Zand <florianzand@me.com>
2024-01-18 16:43:23 +0100

    Update ObserverView.swift

--- Sources/FZUIKit/Classes/AppKit/Views/ObserverView.swift
+++ Sources/FZUIKit/Classes/AppKit/Views/ObserverView.swift
@@ -55,8 +55,8 @@ extension NSView {
         if windowHandlers.needsObserving || mouseHandlers.needsObserving || viewHandlers.needsObserving || dragAndDropHandlers.isActive {
             if observerView == nil {
                 self.observerView = ObserverView()
-               // addSubview(withConstraint: observerView!)
-                insertSubview(observerView!, at: 0)
+                observerView?.backgroundColor = .red.withAlphaComponent(0.1)
+                addSubview(withConstraint: observerView!)
                 do {
                     try replaceMethod(
                         #selector(NSView.didAddSubview(_:)),
@@ -64,7 +64,7 @@ extension NSView {
                         hookSignature: (@convention(block) (AnyObject, NSView) -> Void).self
                     ) { store in { object, subview in
                         store.original(object, #selector(NSView.didAddSubview(_:)), subview)
-                   //     (object as? NSView)?.observerView?.sendToFront()
+                        (object as? NSView)?.observerView?.sendToFront()
                     }
                     }
                 } catch {
