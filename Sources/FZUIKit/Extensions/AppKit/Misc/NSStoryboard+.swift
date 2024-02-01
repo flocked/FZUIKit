00ef5e4f8bdd48f4e35b2217d4a094b531d8dd13
Florian Zand <florianzand@me.com>
2024-02-01 06:02:56 +0100

    Update NSStoryboard+.swift

--- Sources/FZUIKit/Extensions/AppKit/Misc/NSStoryboard+.swift
+++ Sources/FZUIKit/Extensions/AppKit/Misc/NSStoryboard+.swift
@@ -5,17 +5,31 @@
 //  Created by Florian Zand on 04.05.23.
 //
 
-#if os(macOS)
+#if os(macOS) || os(iOS) || os(tvOS)
+    #if os(macOS)
     import AppKit
+    #else
+    import UIKit
+    #endif
 
-    public extension NSStoryboard {
+    public extension NSUIStoryboard {
         /**
-         Creates a storyboard based on the named storyboard file in the specified bundle.
+         Creates a storyboard based on the specified name in the main bundle.
 
-         - Parameter name: The name of the storyboard file, without the filename extension. This method raises an exception if this parameter’s value is `nil.
+         - Parameter name: The name of the storyboard file, without the filename extension.
          - Returns: A new storyboard object.
          */
         convenience init(name: NSStoryboard.Name) {
+            self.init(name: name, bundle: nil)
+        }
+        
+        /**
+         Creates a storyboard based on the specified name in the main bundle.
+
+         - Parameter name: The name of the storyboard file, without the filename extension.
+         - Returns: A new storyboard object.
+         */
+        convenience init(_ name: NSStoryboard.Name) {
             self.init(name: name, bundle: nil)
         }
     }
