79a012467231e1315bd11be94ce087b341614c0d
Florian Zand <florianzand@me.com>
2024-02-03 17:03:51 +0100

    Update NSPopover+.swift

--- Sources/FZUIKit/Extensions/AppKit/Misc/NSPopover+.swift
+++ Sources/FZUIKit/Extensions/AppKit/Misc/NSPopover+.swift
@@ -97,7 +97,7 @@
             - positioningRect: The rectangle within `positioningView` relative to which the popover should be positioned. Normally set to the bounds of `positioningView`. May be an empty rectangle, which will default to the bounds of positioningView.
             - positioningView: The view relative to which the popover should be positioned. Causes the method to raise `invalidArgumentException if `nil`.
             - preferredEdge: The edge of positioningView the popover should prefer to be anchored to.
-            - trackViewFrame: A Boolean value that indicates whether to automatically reposition the popover when the view frame changes.
+            - trackViewFrame: A Boolean value that indicates whether to automatically reposition the popover when the positioning view's frame changes.
          */
         public func show(relativeTo positioningRect: CGRect, of positioningView: NSView, preferredEdge: NSRectEdge, trackViewFrame: Bool) {
             show(relativeTo: positioningRect, of: positioningView, preferredEdge: preferredEdge, hideArrow: false, trackViewFrame: trackViewFrame)
@@ -111,7 +111,7 @@
             - positioningView: The view relative to which the popover should be positioned. Causes the method to raise `invalidArgumentException` if `nil`.
             - preferredEdge: The edge of positioningView the popover should prefer to be anchored to.
             - hideArrow: A Boolean value that indicates whether to hide the arrow of the popover.
-            - trackViewFrame: A Boolean value that indicates whether to automatically reposition the popover when the view frame changes.
+            - trackViewFrame: A Boolean value that indicates whether to automatically reposition the popover when the positioning view's frame changes.
          */
         public func show(relativeTo positioningRect: CGRect, of positioningView: NSView, preferredEdge: NSRectEdge, hideArrow: Bool, trackViewFrame: Bool = true) {
             isOpeningPopover = true
@@ -133,6 +133,12 @@
                 }
                 self.noArrowView = noArrowView
                 positioningView.superview?.addSubview(noArrowView)
+                var preferredEdge = preferredEdge
+                if preferredEdge == .minY {
+                    preferredEdge = .maxY
+                } else if preferredEdge == .maxY {
+                    preferredEdge = .minY
+                }
                 show(relativeTo: positioningRect, of: noArrowView, preferredEdge: preferredEdge)
                 noArrowView.frame = NSRect(x: 0, y: -200, width: 10, height: 10)
                 willCloseObserver = NotificationCenter.default.observe(NSPopover.willCloseNotification, object: self, using: { notification in
