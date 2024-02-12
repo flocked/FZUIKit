24fdba8adad0eb977a84bff9023a5cd979dc33d1
Florian Zand <florianzand@me.com>
2024-02-12 17:04:36 +0100

    Update NSFilePromiseProvider+.swift

--- Sources/FZUIKit/Extensions/AppKit/Pasteboard/NSFilePromiseProvider+.swift
+++ Sources/FZUIKit/Extensions/AppKit/Pasteboard/NSFilePromiseProvider+.swift
@@ -19,8 +19,21 @@ open class FilePromiseProvider: NSFilePromiseProvider, NSFilePromiseProviderDele
     /// The file name for writing the file.
     open var fileName: String
     
+    /// The handler for writing the file.
     public let handler: Handler
-            
+    
+    /// The operation queue from which to issue the write request.
+    public var operationQueue: OperationQueue = .main
+    
+    
+    /**
+     Initializes a file promise provider that calls the specified handler for writing the file.
+     
+     - Parameters:
+        - fileName: the drag destination file's name.
+        - fileType: The file content type (`UTI`).
+        - handler: The handler for writing the file.
+     */
     public init(fileName: String, fileType: String, handler: @escaping Handler) {
         self.fileName = fileName
         self.handler = handler
@@ -29,13 +42,132 @@ open class FilePromiseProvider: NSFilePromiseProvider, NSFilePromiseProviderDele
         self.delegate = self
     }
     
+    /**
+     Initializes a file promise provider that calls the specified handler for writing the file.
+     
+     - Parameters:
+        - fileName: the drag destination file's name.
+        - fileType: The file content type.
+        - handler: The handler for writing the file.
+     */
     @available(macOS 11.0, *)
-    public init(fileName: String, fileType: UTType, handler: @escaping Handler) {
-        self.fileName = fileName
-        self.handler = handler
-        super.init()
-        self.fileType = fileType.identifier
-        self.delegate = self
+    public convenience init(fileName: String, fileType: UTType, handler: @escaping Handler) {
+        self.init(fileName: fileName, fileType: fileType.identifier, handler: handler)
+    }
+    
+    /**
+     Initializes a file promise provider that writes the specified data.
+     
+     - Parameters:
+        - data: The data to write.
+        - fileName: the drag destination file's name.
+        - fileType: The file content type (`UTI`).
+        - existingFileStrategy: The strategy for handling an existing file.
+     */
+    public convenience init(data: Data, fileName: String, fileType: String, existingFileStrategy: ExistingFileStrategy = .skip) {
+        self.init(fileName: fileName, fileType: fileType) { url, completionHandler in
+            do {
+                let url = try Self.checkExistingFile(url: url, fileName: fileName, strategy: existingFileStrategy)
+                try data.write(to: url, options: [.atomic])
+                completionHandler(nil)
+            } catch {
+                completionHandler(error)
+            }
+        }
+    }
+    
+    /**
+     Initializes a file promise provider that writes the specified data.
+     
+     - Parameters:
+        - data: The data to write.
+        - fileName: the drag destination file's name.
+        - fileType: The file content type.
+        - existingFileStrategy: The strategy for handling an existing file.
+     */
+    @available(macOS 11.0, *)
+    public convenience init(data: Data, fileName: String, fileType: UTType, existingFileStrategy: ExistingFileStrategy = .skip) {
+        self.init(data: data, fileName: fileName, fileType: fileType.identifier, existingFileStrategy: existingFileStrategy)
+    }
+    
+    /**
+     Initializes a file promise provider that writes the specified codable type.
+     
+     - Parameters:
+        - content: The codable type to write.
+        - fileName: the drag destination file's name.
+        - fileType: The file content type (`UTI`).
+        - existingFileStrategy: The strategy for handling an existing file.
+     */
+    public convenience init<Content: Encodable>(content: Content, fileName: String, fileType: String, existingFileStrategy: ExistingFileStrategy = .skip) {
+        self.init(fileName: fileName, fileType: fileType) { url, completionHandler in
+            do {
+                let url = try Self.checkExistingFile(url: url, fileName: fileName, strategy: existingFileStrategy)
+                let data = try JSONEncoder().encode(content)
+                try data.write(to: url, options: [.atomic])
+                completionHandler(nil)
+            } catch {
+                completionHandler(error)
+            }
+        }
+    }
+    
+    /**
+     Initializes a file promise provider that writes the specified codable type.
+     
+     - Parameters:
+        - content: The codable type to write.
+        - fileName: the drag destination file's name.
+        - fileType: The file content type.
+        - existingFileStrategy: The strategy for handling an existing file.
+     */
+    @available(macOS 11.0, *)
+    public convenience init<Content: Encodable>(content: Content, fileName: String, fileType: UTType, existingFileStrategy: ExistingFileStrategy = .skip) {
+        self.init(content: content, fileName: fileName, fileType: fileType.identifier, existingFileStrategy: existingFileStrategy)
+    }
+    
+    /// The strategy for handling an existing file.
+    public enum ExistingFileStrategy {
+        /// Skips writing the file.
+        case skip
+        /// Overwriting the file.
+        case overwrite
+        /// Renames the existing file.
+        case renameExisting((String)->(String))
+        /// Renames the new file.
+        case renameNew((String)->(String))
+    }
+    
+    /// File promise provider errors.
+    public enum Errors: Error {
+        /// The file already exists at the specified url.
+        case fileExistsAlready(URL)
+    }
+    
+    static func checkExistingFile(url: URL, fileName: String, strategy: ExistingFileStrategy) throws -> URL {
+        var url = url
+        var shouldWrite = true
+        if FileManager.default.fileExists(at: url) {
+            switch strategy {
+            case .skip:
+                shouldWrite = false
+            case .overwrite:
+                break
+            case .renameExisting(let handler):
+                let newURL = url.deletingLastPathComponent().appendingPathComponent(handler(fileName))
+                shouldWrite = FileManager.default.fileExists(at: newURL) == false
+                if shouldWrite {
+                    try FileManager.default.moveItem(at: url, to: newURL)
+                }
+            case .renameNew(let handler):
+                url = url.deletingLastPathComponent().appendingPathComponent(handler(fileName))
+                shouldWrite = !FileManager.default.fileExists(at: url)
+            }
+        }
+        guard shouldWrite else {
+            throw Errors.fileExistsAlready(url)
+        }
+        return url
     }
     
     open func filePromiseProvider(_ filePromiseProvider: NSFilePromiseProvider, fileNameForType fileType: String) -> String {
@@ -45,181 +177,22 @@ open class FilePromiseProvider: NSFilePromiseProvider, NSFilePromiseProviderDele
     open func filePromiseProvider(_ filePromiseProvider: NSFilePromiseProvider, writePromiseTo url: URL, completionHandler: @escaping (Error?) -> Void) {
         handler(url, completionHandler)
     }
-}
-
-/// A file promise provider that writes a codable type.
-open class CodableFilePromiseProvider<Content: Codable>: NSFilePromiseProvider, NSFilePromiseProviderDelegate {
-    /// The content to write.
-    public let content: Content
     
-    /// The file name for writing the file.
-    open var fileName: String
-    
-    /// The strategy for handling an existing file.
-    open var existingFileStrategy: ExistingFileStrategy = .skip
-    
-    /// The strategy for handling an existing file.
-    public enum ExistingFileStrategy {
-        /// Skips writing the file.
-        case skip
-        /// Overwriting the file.
-        case overwrite
-        /// Renames the existing file.
-        case renameExisting((String)->(String))
-        /// Renames the new file.
-        case renameNew((String)->(String))
-    }
-    
-    public enum FilePromiseError: Error {
-        /// The file already exists at the specified url.
-        case fileExistsAlready(URL)
-    }
-    
-    public init(content: Content, fileName: String, fileType: String) {
-        self.content = content
-        self.fileName = fileName
-        super.init()
-        self.fileType = fileType
-        self.delegate = self
-    }
-    
-    @available(macOS 11.0, *)
-    public init(content: Content, fileName: String, fileType: UTType) {
-        self.content = content
-        self.fileName = fileName
-        super.init()
-        self.fileType = fileType.identifier
-        self.delegate = self
-    }
-    
-    open func filePromiseProvider(_ filePromiseProvider: NSFilePromiseProvider, fileNameForType fileType: String) -> String {
-        return fileName
-    }
-    
-    open func filePromiseProvider(_ filePromiseProvider: NSFilePromiseProvider, writePromiseTo url: URL, completionHandler: @escaping (Error?) -> Void) {
-        do {
-            var url = url
-            var shouldWrite = true
-            if FileManager.default.fileExists(at: url) {
-                switch existingFileStrategy {
-                case .skip:
-                    shouldWrite = false
-                case .overwrite:
-                    break
-                case .renameExisting(let handler):
-                    let newURL = url.deletingLastPathComponent().appendingPathComponent(handler(fileName))
-                    shouldWrite = FileManager.default.fileExists(at: newURL) == false
-                    if shouldWrite {
-                        try FileManager.default.moveItem(at: url, to: newURL)
-                    }
-                case .renameNew(let handler):
-                    url = url.deletingLastPathComponent().appendingPathComponent(handler(fileName))
-                    shouldWrite = !FileManager.default.fileExists(at: url)
-                }
-            }
-            if shouldWrite {
-                let data = try JSONEncoder().encode(content)
-                try data.write(to: url, options: [.atomic])
-                completionHandler(nil)
-            } else {
-                completionHandler(FilePromiseError.fileExistsAlready(url))
-            }
-        } catch {
-            completionHandler(error)
-        }
+    open func operationQueue(for filePromiseProvider: NSFilePromiseProvider) -> OperationQueue {
+        return operationQueue
     }
 }
 
 public extension FileConvertible {
-    func filePromiseProvider(fileName: String, fileType: String) -> CodableFilePromiseProvider<Self> {
-        CodableFilePromiseProvider(content: self, fileName: fileName, fileType: fileType)
+    /// Creates a file promise provider for the receiver.
+    func filePromiseProvider(fileName: String, fileType: String, existingFileStrategy: FilePromiseProvider.ExistingFileStrategy = .skip) -> FilePromiseProvider {
+        FilePromiseProvider(content: self, fileName: fileName, fileType: fileType, existingFileStrategy: existingFileStrategy)
     }
     
+    /// Creates a file promise provider for the receiver.
     @available(macOS 11.0, *)
-    func filePromiseProvider(fileName: String, fileType: UTType) -> CodableFilePromiseProvider<Self> {
-        CodableFilePromiseProvider(content: self, fileName: fileName, fileType: fileType)
-    }
-}
-
-/// A file promise provider that writes data.
-open class DataFilePromiseProvider: NSFilePromiseProvider, NSFilePromiseProviderDelegate {
-    /// The data to write.
-    public let data: Data
-    
-    /// The file name for writing the file.
-    open var fileName: String
-    
-    /// The strategy for handling an existing file.
-    open var existingFileStrategy: ExistingFileStrategy = .skip
-    
-    /// The strategy for handling an existing file.
-    public enum ExistingFileStrategy {
-        /// Skips writing the file.
-        case skip
-        /// Overwriting the file.
-        case overwrite
-        /// Renames the existing file.
-        case renameExisting((String)->(String))
-        /// Renames the new file.
-        case renameNew((String)->(String))
-    }
-    
-    public enum FilePromiseError: Error {
-        /// The file already exists at the specified url.
-        case fileExistsAlready(URL)
-    }
-    
-    public init(data: Data, fileName: String, fileType: String) {
-        self.data = data
-        self.fileName = fileName
-        super.init()
-        self.fileType = fileType
-        self.delegate = self
-    }
-    
-    @available(macOS 11.0, *)
-    public init(data: Data, fileName: String, fileType: UTType) {
-        self.data = data
-        self.fileName = fileName
-        super.init()
-        self.fileType = fileType.identifier
-        self.delegate = self
-    }
-    
-    open func filePromiseProvider(_ filePromiseProvider: NSFilePromiseProvider, fileNameForType fileType: String) -> String {
-        return fileName
-    }
-    
-    open func filePromiseProvider(_ filePromiseProvider: NSFilePromiseProvider, writePromiseTo url: URL, completionHandler: @escaping (Error?) -> Void) {
-        do {
-            var url = url
-            var shouldWrite = true
-            if FileManager.default.fileExists(at: url) {
-                switch existingFileStrategy {
-                case .skip:
-                    shouldWrite = false
-                case .overwrite:
-                    break
-                case .renameExisting(let handler):
-                    let newURL = url.deletingLastPathComponent().appendingPathComponent(handler(fileName))
-                    shouldWrite = FileManager.default.fileExists(at: newURL) == false
-                    if shouldWrite {
-                        try FileManager.default.moveItem(at: url, to: newURL)
-                    }
-                case .renameNew(let handler):
-                    url = url.deletingLastPathComponent().appendingPathComponent(handler(fileName))
-                    shouldWrite = !FileManager.default.fileExists(at: url)
-                }
-            }
-            if shouldWrite {
-                try data.write(to: url, options: [.atomic])
-                completionHandler(nil)
-            } else {
-                completionHandler(FilePromiseError.fileExistsAlready(url))
-            }
-        } catch {
-            completionHandler(error)
-        }
+    func filePromiseProvider(fileName: String, fileType: UTType, existingFileStrategy: FilePromiseProvider.ExistingFileStrategy = .skip) -> FilePromiseProvider {
+        FilePromiseProvider(content: self, fileName: fileName, fileType: fileType, existingFileStrategy: existingFileStrategy)
     }
 }
 #endif
