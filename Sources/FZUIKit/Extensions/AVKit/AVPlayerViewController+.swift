cdf5817e43b17fe65ba1a4af5c7795ef61272d07
Florian Zand <florianzand@me.com>
2024-02-22 08:48:41 +0100

    Update AVPlayerViewController+.swift

--- Sources/FZUIKit/Extensions/AVKit/AVPlayerViewController+.swift
+++ Sources/FZUIKit/Extensions/AVKit/AVPlayerViewController+.swift
@@ -5,7 +5,7 @@
 //  Created by Florian Zand on 22.02.24.
 //
 
-#if os(iOS)
+#if os(iOS) || os(tvOS)
 import AVKit
 import FZSwiftUtils
 
@@ -31,17 +31,59 @@ extension AVPlayerViewController {
         }
         overlayView.frame = videoBounds
         set(associatedValue: overlayView, key: "resizingContentOverlayView", object: self)
+        #if os(iOS)
         videoBoundsObservation = observeChanges(for: \.videoBounds, handler: { [weak self] old, new in
             guard let self = self, old != new else { return }
             self.resizingContentOverlayView.frame = new
         })
+        #else
+        videoViewControllerObserver?.add(\.view?.frame) { [weak self] old, new in
+            guard let self = self, old != new else { return }
+            self.resizingContentOverlayView.frame = self.videoBounds
+        }
+        videoViewControllerObserver?.add(\.videoGravity) { [weak self] old, new in
+            guard let self = self, old != new else { return }
+            self.resizingContentOverlayView.frame = self.videoBounds
+        }
+        videoViewControllerObserver?.add(\.player?.currentItem) { [weak self] old, new in
+            guard let self = self, old != new else { return }
+            self.resizingContentOverlayView.frame = self.videoBounds
+        }
+        #endif
         return overlayView
     }
     
+    #if os(iOS)
     var videoBoundsObservation: NSKeyValueObservation? {
         get { getAssociatedValue(key: "videoBoundsObservation", object: self, initialValue: nil) }
         set { set(associatedValue: newValue, key: "videoBoundsObservation", object: self) }
     }
+    #else
+    var videoViewControllerObserver: KeyValueObserver<AVPlayerViewController>? {
+        get { getAssociatedValue(key: "videoBoundsObservation", object: self, initialValue: KeyValueObserver(self)) }
+    }
+    
+    /**
+     The size and position of the video image within the bounds of the view controller’s view.
+ 
+     The size and position of this rectangle depend on the aspect ratio of the media (like 16:9 or 4:3), the bounds of the player view controller’s view, and the view controller’s `videoGravity`.
+     */
+    public var videoBounds: CGRect {
+        guard var videoSize = player?.currentItem?.asset.videoNaturalSize, let frameSize = view?.frame.size else {
+            return .zero
+        }
+        switch videoGravity {
+        case .resizeAspect:
+            videoSize = videoSize.scaled(toFit: frameSize)
+        case .resizeAspectFill:
+            videoSize = videoSize.scaled(toFill: frameSize)
+        default:
+            return CGRect(.zero, frameSize)
+        }
+        var rect = CGRect(.zero, videoSize)
+        rect.center = CGRect(.zero, frameSize).center
+        return rect
+    }
+    #endif
 }
-
 #endif
