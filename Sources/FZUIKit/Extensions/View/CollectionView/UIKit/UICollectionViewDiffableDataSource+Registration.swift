7cdac3ca89590da68df0b11d21d740464f9a7bbf
Florian Zand <florianzand@me.com>
2024-02-26 11:10:34 +0100

    updated

--- Sources/FZUIKit/Extensions/View/CollectionView/UIKit/UICollectionViewDiffableDataSource+Registration.swift
+++ Sources/FZUIKit/Extensions/View/CollectionView/UIKit/UICollectionViewDiffableDataSource+Registration.swift
@@ -21,42 +21,5 @@ public extension UICollectionViewDiffableDataSource {
             collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: item)
         }
     }
-    
-    /**
-     Updates the UI to reflect the state of the data in the specified snapshot, optionally animating the UI changes and executing a completion handler.
-
-     It’s safe to call this method from a background queue, but you must do so consistently in your app. Always call this method exclusively from the main queue or from a background queue.
-
-     - Parameters:
-        - snapshot: The snapshot reflecting the new state of the data in the collection view.
-        - option: Option how to apply the snapshot to the table view.
-        - completion: An optional closure to be executed when the animations are complete. This closure has no return value and takes no parameters. The system calls this closure from the main queue. The default value is `nil`.
-     */
-    func apply(_ snapshot: NSDiffableDataSourceSnapshot<SectionIdentifierType, ItemIdentifierType>, _ option: NSDiffableDataSourceSnapshotApplyOption, completion: (() -> Void)? = nil) {
-        switch option {
-        case .usingReloadData:
-            if #available(iOS 15.0, tvOS 15.0, *) {
-                applySnapshotUsingReloadData(snapshot, completion: completion)
-            } else {
-                apply(snapshot, animatingDifferences: false, completion: completion)
-            }
-        case .animated(duration: let duration):
-            if duration == NSDiffableDataSourceSnapshotApplyOption.noAnimationDuration {
-                apply(snapshot, animatingDifferences: true, completion: completion)
-            } else {
-                UIView.animate(withDuration: duration) {
-                    self.apply(snapshot, animatingDifferences: true, completion: completion)
-                }
-            }
-        case .withoutAnimation:
-            if #available(iOS 15.0, tvOS 15.0, *) {
-                apply(snapshot, animatingDifferences: false, completion: completion)
-            } else {
-                UIView.animate(withDuration: 0.0) {
-                    self.apply(snapshot, animatingDifferences: true, completion: completion)
-                }
-            }
-        }
-    }
 }
 #endif
