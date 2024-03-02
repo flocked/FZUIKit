1cd860f1eaa5c364025426870593e73e12ec83b5
Florian Zand <florianzand@me.com>
2024-03-02 07:43:44 +0100

    Update NSTextField+AdjustFont.swift

--- Sources/FZUIKit/Extensions/View/AppKit/NSTextField/NSTextField+AdjustFont.swift
+++ Sources/FZUIKit/Extensions/View/AppKit/NSTextField/NSTextField+AdjustFont.swift
@@ -327,10 +327,11 @@
                         methodSignature: (@convention(c) (AnyObject, Selector, Notification) -> Void).self,
                         hookSignature: (@convention(block) (AnyObject, Notification) -> Void).self
                     ) { store in { object, notification in
-                      //  store.original(object, #selector(NSTextField.textDidEndEditing), notification)
+                        store.original(object, #selector(NSTextField.textDidEndEditing), notification)
                         if let textField = (object as? NSTextField) {
                             //  textField.editingState = .didEnd
                             textField.adjustFontSize()
+                            textField.editingHandlers.didEnd?()
                             if textField.isEditableByDoubleClick {
                                 textField.isSelectable = textField._isSelectable
                                 textField.isEditable = textField._isEditable
@@ -339,7 +340,6 @@
                                 textField.sizeToFit()
                             }
                             textField.invalidateIntrinsicContentSize()
-                            textField.editingHandlers.didEnd?()
                         }
                     }
                     })
