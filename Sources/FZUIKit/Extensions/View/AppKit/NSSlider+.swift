6096a1ad6b4fa5b8f274e5ec3a8c70a603790639
Florian Zand <florianzand@me.com>
2024-03-07 16:59:18 +0100

    Update NSSlider+.swift

--- Sources/FZUIKit/Extensions/View/AppKit/NSSlider+.swift
+++ Sources/FZUIKit/Extensions/View/AppKit/NSSlider+.swift
@@ -11,6 +11,16 @@
     import Foundation
 
     public extension NSSlider {
+        /// The rectangle in which the bar is drawn.
+        var barRect: CGRect {
+            (cell as? NSSliderCell)?.barRect(flipped: isFlipped) ?? .zero
+        }
+        
+        /// The rectangle in which the knob is drawn.
+        var knobRect: CGRect {
+            (cell as? NSSliderCell)?.knobRect(flipped: isFlipped) ?? .zero
+        }
+        
         /// The position of the slider knob.
         var knobPointPosition: CGFloat {
             let sliderOrigin = frame.origin.x + knobThickness / 2
