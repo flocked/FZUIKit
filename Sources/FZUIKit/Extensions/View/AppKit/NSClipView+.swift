51645d4a7b3d4e12404638e0c8233fc45a969f08
Florian Zand <florianzand@me.com>
2024-03-16 23:25:34 +0100

    updated

--- Sources/FZUIKit/Extensions/View/AppKit/NSClipView+.swift
+++ Sources/FZUIKit/Extensions/View/AppKit/NSClipView+.swift
@@ -9,14 +9,13 @@
 import AppKit
 import FZSwiftUtils
 
-
 extension NSClipView {
     /**
      Changes the origin of the clip view’s bounds rectangle animted to newOrigin.
-
+     
      - Parameters:
-        - newOrigin: The point in the view to scroll to.
-        - animationDuration: The animation duration of the scolling.
+     - newOrigin: The point in the view to scroll to.
+     - animationDuration: The animation duration of the scolling.
      */
     func scroll(to newOrigin: CGPoint, animationDuration: CGFloat) {
         if animationDuration > 0.0 {
@@ -29,6 +28,76 @@ extension NSClipView {
         } else {
             scroll(to: newOrigin)
         }
+    }
+    
+    /// A Boolean value indicating whether users can scroll the view by dragging the mouse.
+    @objc open var isScrollableByDagging: Bool {
+        get { dragScrollGestureRecognizer != nil }
+        set {
+            guard newValue != isScrollableByDagging else { return }
+            if newValue {
+                dragScrollGestureRecognizer = DragScrollGestureRecognizer()
+                addGestureRecognizer(dragScrollGestureRecognizer!)
+            } else {
+                dragScrollGestureRecognizer?.removeFromView()
+                dragScrollGestureRecognizer = nil
+            }
+        }
+    }
+    
+    var dragScrollGestureRecognizer: DragScrollGestureRecognizer? {
+        get { getAssociatedValue(key: "dragScrollGestureRecognizer", object: self, initialValue: nil) }
+        set { set(associatedValue: newValue, key: "dragScrollGestureRecognizer", object: self)
+        }
+    }
+    
+    class DragScrollGestureRecognizer: NSGestureRecognizer {
+        
+        var clipView: NSClipView? { view as? NSClipView }
+        var clickPoint: CGPoint = .zero
+        var originalOrigin: CGPoint = .zero
+        
+        override func mouseDown(with event: NSEvent) {
+            guard let clipView = clipView, clipView.isScrollableByDagging else {
+                super.mouseDown(with: event)
+                return
+            }
+            clickPoint = event.locationInWindow
+            originalOrigin = clipView.bounds.origin
+        }
+        
+        override func mouseDragged(with event: NSEvent) {
+            guard let clipView = clipView, clipView.isScrollableByDagging else {
+                super.mouseDragged(with: event)
+                return
+            }
+            let scale = (clipView.superview as? NSScrollView)?.magnification ?? 1.0
+            let newPoint = event.locationInWindow
+            let newOrigin = CGPoint(x: originalOrigin.x + (clickPoint.x - newPoint.x) / scale,
+                                    y: originalOrigin.y + (clickPoint.y - newPoint.y) / scale)
+            clipView.scroll(to: newOrigin)
+            clipView.superview?.reflectScrolledClipView(clipView)
+        }
+        
+        convenience init() {
+            self.init(target: nil, action: nil)
+        }
+        
+        override init(target: Any?, action: Selector?) {
+            super.init(target: target, action: action)
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    }
+}
+
+extension NSScrollView {
+    /// A Boolean value indicating whether users can scroll the view by dragging the mouse.
+    @objc open var isScrollableByDagging: Bool {
+        get { contentView.isScrollableByDagging }
+        set { contentView.isScrollableByDagging = newValue }
     }
 }
 
