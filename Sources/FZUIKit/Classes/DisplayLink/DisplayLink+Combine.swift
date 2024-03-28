a807c89e789e5adaa1d627704c5ceba02882d9c2
Florian Zand <florianzand@me.com>
2024-03-28 23:36:31 +0100

    Update DisplayLink+Combine.swift

--- Sources/FZUIKit/Classes/DisplayLink/DisplayLink+Combine.swift
+++ Sources/FZUIKit/Classes/DisplayLink/DisplayLink+Combine.swift
@@ -247,9 +247,6 @@
                             timestamp: inNow.pointee.timeInterval,
                             duration: (Double(inOutputTime.pointee.timeInterval) - Double(inNow.pointee.timeInterval)) / 2.0
                         )
-                        let timeStamp = inNow.pointee
-                        
-                        Swift.print(timeStamp.videoTimeScale, timeStamp.videoTime, timeStamp.videoRefreshPeriod, timeStamp.rateScalar, timeStamp.hostTime)
                         DispatchQueue.main.async {
                             self?.handle(frame: frame)
                         }
