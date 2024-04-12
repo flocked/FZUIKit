701e8e45d73cf06401fbfda9a523f6cc5dfcd084
Florian Zand <florianzand@me.com>
2024-04-12 20:18:03 +0200

    Update DelegateProxyType.swift

--- Sources/FZUIKit/Classes/DelegateProxy/DelegateProxyType.swift
+++ Sources/FZUIKit/Classes/DelegateProxy/DelegateProxyType.swift
@@ -16,7 +16,31 @@ public protocol DelegateProxyType {
     func setDelegate(to object: Object)
 }
 
+
+
 public extension DelegateProxyType where Self: DelegateProxy {
+    static func create<Delegate>(for object: Object, keyPath: WritableKeyPath<Object, Delegate?>) -> Self {
+        objc_sync_enter(self)
+        defer { objc_sync_exit(self) }
+        
+        let delegateProxy = object.getAssociatedValue("_delegateProxy", initialValue: Self.init())
+        delegateProxy.setDelegate(to: object)
+        
+        let observer = object.getAssociatedValue("_delegateProxyObserver", initialValue: KeyValueObserver<Object>(object))
+        if let keyPath = keyPath._kvcKeyPathString, !observer.isObserving(keyPath) {
+            observer.add(keyPath) { old, new, _ in
+                if let old = old as? NSObject, let new = new as? NSObject {
+                    guard old != new, new != delegateProxy else { return }
+                    delegateProxy.setDelegate(to: object)
+                } else if (new as? NSObject) != delegateProxy {
+                    delegateProxy.setDelegate(to: object)
+                }
+            }
+        }
+        
+        return delegateProxy
+    }
+    
     static func create(for object: Object) -> Self {
         objc_sync_enter(self)
         defer { objc_sync_exit(self) }
