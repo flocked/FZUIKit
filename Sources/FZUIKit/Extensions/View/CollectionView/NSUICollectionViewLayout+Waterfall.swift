4025ce2f3c7d7de8dc32a28ecc1b6a78e65eb3c4
Florian Zand <florianzand@me.com>
2024-04-13 10:17:53 +0200

    Update NSUICollectionViewLayout+Waterfall.swift

--- Sources/FZUIKit/Extensions/View/CollectionView/NSUICollectionViewLayout+Waterfall.swift
+++ Sources/FZUIKit/Extensions/View/CollectionView/NSUICollectionViewLayout+Waterfall.swift
@@ -608,6 +608,7 @@ public class CollectionViewWaterfallLayout: NSUICollectionViewLayout, PinchableC
     override public func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
         if newBounds.width != currentBounds.width {
             currentBounds = newBounds
+            Swift.print("shouldInvalidateLayout", collectionView?.displayingIndexPaths(in: self.contentViewBounds).compactMap({$0.item}).sorted() ?? [])
             return true
         }
         currentBounds = newBounds
