f487977d6d228f232abe0ce382ab446770c9cf86
Florian Zand <florianzand@me.com>
2024-04-22 06:51:51 +0200

    Update NSWorkspace+.swift

--- Sources/FZUIKit/Extensions/AppKit/Misc/NSWorkspace+.swift
+++ Sources/FZUIKit/Extensions/AppKit/Misc/NSWorkspace+.swift
@@ -39,8 +39,22 @@ public extension NSWorkspace.OpenConfiguration {
     
     /// Sets the Boolean value indicating whether to display errors, authentication requests, or other UI elements to the user.
     @discardableResult
-    func promptsUserIfNeeded(_ promptsUserIfNeeded: Bool) -> Self {
-        self.promptsUserIfNeeded = promptsUserIfNeeded
+    func promptsUserIfNeeded(_ prompts: Bool) -> Self {
+        promptsUserIfNeeded = prompts
+        return self
+    }
+    
+    /// Sets the Boolean value indicating whether you want to print the contents of documents and URLs instead of opening them.
+    @discardableResult
+    func isForPrinting(_ isForPrinting: Bool) -> Self {
+        self.isForPrinting = isForPrinting
+        return self
+    }
+    
+    /// Sets the Boolean value indicating whether you require the URL to have an associated universal link.
+    @discardableResult
+    func requiresUniversalLinks(_ requires: Bool) -> Self {
+        requiresUniversalLinks = requires
         return self
     }
     
@@ -48,6 +62,27 @@ public extension NSWorkspace.OpenConfiguration {
     @discardableResult
     func arguments(_ arguments: [String]) -> Self {
         self.arguments = arguments
+        return self
+    }
+    
+    /// Sets the first Apple event to send to the new app.
+    @discardableResult
+    func appleEvent(_ appleEvent: NSAppleEventDescriptor?) -> Self {
+        self.appleEvent = appleEvent
+        return self
+    }
+    
+    /// Sets the architecture version of the app to launch.
+    @discardableResult
+    func architecture(_ architecture: cpu_type_t) -> Self {
+        self.architecture = architecture
+        return self
+    }
+    
+    /// Sets the set of environment variables to set in a new app instance.
+    @discardableResult
+    func environment(_ environment: [String : String]) -> Self {
+        self.environment = environment
         return self
     }
 }
