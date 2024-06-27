12a29883bded387339f73d4a68ec1af9bf272ebb
Florian Zand <florianzand@me.com>
2024-06-27 05:27:45 +0200

    Update NSSlider+.swift

--- Sources/FZUIKit/Extensions/View/AppKit/NSSlider+.swift
+++ Sources/FZUIKit/Extensions/View/AppKit/NSSlider+.swift
@@ -29,6 +29,89 @@
             let knobPos = sliderOrigin + sliderWidth * CGFloat((doubleValue - minValue) / (maxValue - minValue))
             return knobPos
         }
+        
+        /// Sets the minimum value the slider can send to its target.
+        @discardableResult
+        func minValue(_ minValue: Double) -> Self {
+            self.minValue = minValue
+            return self
+        }
+        
+        /// Sets the maximum value the slider can send to its target.
+        @discardableResult
+        func maxValue(_ maxValue: Double) -> Self {
+            self.maxValue = maxValue
+            return self
+        }
+        
+        /// Sets the amount by which the slider changes its value when the user Option-drags the slider knob.
+        @discardableResult
+        func altIncrementValue(_ value: Double) -> Self {
+            self.altIncrementValue = value
+            return self
+        }
+        
+        /// Sets the orientaiton of the slider.
+        @discardableResult
+        func orientation(_ orientation: NSUserInterfaceLayoutOrientation) -> Self {
+            self.isVertical = orientation == .vertical
+            return self
+        }
+        
+        /// Sets the type of the slider, such as vertical or circular.
+        @discardableResult
+        func type(_ type: SliderType) -> Self {
+            self.sliderType = type
+            return self
+        }
+        
+        /// Sets the color of the filled portion of the slider track, in appearances that support it.
+        @discardableResult
+        func trackFillColor(_ color: NSColor?) -> Self {
+            self.trackFillColor = color
+            return self
+        }
+        
+        /// Sets the Boolean value that indicates whether the slider fixes its values to those values represented by its tick marks.
+        @discardableResult
+        func allowsTickMarkValuesOnly(_ allows: Bool) -> Self {
+            self.allowsTickMarkValuesOnly = allows
+            return self
+        }
+        
+        /// Sets the number of tick marks associated with the slider.
+        @discardableResult
+        func numberOfTickMarks(_ numberOfTickMarks: Int) -> Self {
+            self.numberOfTickMarks = numberOfTickMarks
+            return self
+        }
+        
+        /**
+         Creates a linear slider with the specified orientation and size.
+         
+         - Parameters:
+            - orientation: The orientation of the slider.
+            - size: The size of the slider.
+         */
+        static func linear(orientation: NSUserInterfaceLayoutOrientation = .horizontal, size: ControlSize = .regular) -> NSSlider {
+            let slider = NSSlider()
+            slider.sliderType = .linear
+            slider.isVertical = orientation == .vertical
+            slider.controlSize = size
+            return slider
+        }
+        
+        /**
+         Creates a circular slider with the specified size.
+         
+         - Parameter size: The size of the slider.
+         */
+        static func circular(size: ControlSize = .regular) -> NSSlider {
+            let slider = NSSlider()
+            slider.sliderType = .circular
+            slider.controlSize = size
+            return slider
+        }
     }
 
 #endif
