61b43d15524848eed30a624e31665cf0faf748e4
Florian Zand <florianzand@me.com>
2024-07-02 06:15:17 +0200

    Update NSScrollView+.swift

--- Sources/FZUIKit/Extensions/View/AppKit/NSScrollView+.swift
+++ Sources/FZUIKit/Extensions/View/AppKit/NSScrollView+.swift
@@ -578,16 +578,15 @@
             set {
                 guard newValue != shouldManageDocumentView else { return }
                 if newValue {
-                    documentView?.frame = bounds
                     scrollViewObserver = KeyValueObserver(self)
-                    scrollViewObserver?.add(\.documentView) { [weak self] old, new in
+                    if let documentView = documentView {
+                        documentView.frame = bounds
+                    } else {
+                        scrollViewObserver?.add(\.documentView) { [weak self] old, new in
+                            guard let self = self, old != new, let new = new else { return }
+                            new.frame = self.bounds
+                        }
                     }
-                    /*
-                    scrollViewObserver?.add(\.documentView) { [weak self] old, new in
-                        guard let self = self, old != new, let new = new else { return }
-                        new.frame = self.bounds
-                    }
-                     */
                     scrollViewObserver?.add(\.frame) { [weak self] old, new in
                         guard let self = self, old != new, let documentView = self.documentView else { return }
                         documentView.frame = CGRect(.zero, new.size)
