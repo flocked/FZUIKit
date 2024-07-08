daf521a59c3ce43364e22aa9a1c63b12409015bb
Florian Zand <florianzand@me.com>
2024-07-08 12:24:52 +0200

    Update NSUICollectionViewLayout+Waterfall.swift

--- Sources/FZUIKit/Extensions/View/CollectionView/NSUICollectionViewLayout+Waterfall.swift
+++ Sources/FZUIKit/Extensions/View/CollectionView/NSUICollectionViewLayout+Waterfall.swift
@@ -797,6 +797,7 @@ extension NSUICollectionView {
         var _isPinchable = true
         override var state: NSUIGestureRecognizer.State {
             didSet {
+                Swift.print("state", state.rawValue)
                 switch state {
                 case .began:
                     _isPinchable = isPinchable
