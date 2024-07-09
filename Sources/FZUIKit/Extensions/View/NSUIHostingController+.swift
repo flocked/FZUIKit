de6a49b6c559e04f01d757a879a5e79e666dfc29
Florian Zand <florianzand@me.com>
2024-07-09 10:19:35 +0200

    Update NSUIHostingController+.swift

--- Sources/FZUIKit/Extensions/View/NSUIHostingController+.swift
+++ Sources/FZUIKit/Extensions/View/NSUIHostingController+.swift
@@ -16,75 +16,17 @@
 
     @available(macOS 11.0, iOS 13.0, *)
     public extension NSUIHostingController {
-        /**
-         Creates a hosting controller object that wraps the specified SwiftUI view.
-
-         - Parameters:
-            - ignoreSafeArea: A Boolean value that indicates whether the hosting controller view should ignore save area insets.
-            - rootView: The root view of the SwiftUI view hierarchy that you want to manage using the hosting view controller.
-
-         - Returns: The hosting controller object.
-         */
-        convenience init(ignoreSafeArea: Bool, rootView: Content) {
-            self.init(rootView: rootView)
-
-            if ignoreSafeArea {
-                isSafeAreaInsetsDisabled = true
-            }
-        }
-
-        /**
-         Creates a hosting controller object that wraps the specified SwiftUI view.
-
-         - Parameters:
-            - isTransparent: A Boolean value that indicates whether the hosting controller view is transparent.
-            - ignoreSafeArea: A Boolean value that indicates whether the hosting controller view should ignore save area insets.
-            - rootView: The root view of the SwiftUI view hierarchy that you want to manage using the hosting view controller.
-
-         - Returns: The hosting controller object.
-         */
-        convenience init(isTransparent: Bool, ignoreSafeArea: Bool = false, rootView: Content) {
-            self.init(rootView: rootView)
-
-            if ignoreSafeArea {
-                isSafeAreaInsetsDisabled = true
-            }
-
-            if isTransparent {
-                view.isOpaque = false
-                view.backgroundColor = .clear
-            }
-        }
-        
-        /// A Boolean value that indicates whether the view should ignore save area insets.
-        var isSafeAreaInsetsDisabled: Bool {
+        /// A Boolean value that indicates whether the SwiftUI view ignores the safe area insets.
+        var ignoresSafeArea: Bool {
             get { view.isMethodReplaced(#selector(getter: NSUIView.safeAreaInsets)) }
-            set { setSafeAreaInsets((newValue == true) ? .zero : nil) }
+            set { view.setSafeAreaInsets(newValue ? .zero : nil) }
         }
         
-        /// Sets the Boolean value that indicates whether the view should ignore save area insets.
+        /// Sets the Boolean value that indicates whether the SwiftUI view ignores the safe area insets.
         @discardableResult
-        func ignoreSafeArea(_ ignores: Bool) -> Self {
-            isSafeAreaInsetsDisabled = ignores
+        func ignoresSafeArea(_ ignores: Bool) -> Self {
+            ignoresSafeArea = ignores
             return self
-        }
-
-        internal func setSafeAreaInsets(_ newSafeAreaInsets: NSUIEdgeInsets?) {
-            view.resetMethod(#selector(getter: NSUIView.safeAreaInsets))
-            if let newSafeAreaInsets = newSafeAreaInsets {
-                do {
-                    try view.replaceMethod(
-                        #selector(getter: NSUIView.safeAreaInsets),
-                        methodSignature: (@convention(c)  (AnyObject, Selector) -> (NSUIEdgeInsets)).self,
-                        hookSignature: (@convention(block)  (AnyObject) -> (NSUIEdgeInsets)).self) { store in {
-                           object in
-                           return newSafeAreaInsets
-                        }
-                   }
-                } catch {
-                // handle error
-                }
-            }
         }
         
         /// The minimum size of the view that satisfies the constraints it holds.
@@ -100,61 +42,45 @@
 
     #if canImport(AppKit)
         public extension NSHostingView {
-            /**
-             Creates a hosting view object that wraps the specified SwiftUI view.
-
-             - Parameters:
-                - isTransparent: A Boolean value that indicates whether the view is transparent.
-                - rootView: The root view of the SwiftUI view hierarchy that you want to manage using the hosting view controller.
-
-             - Returns: The hosting view object.
-             */
-            convenience init(isTransparent: Bool, rootView: Content) {
-                self.init(rootView: rootView)
-                if isTransparent {
-                    isOpaque = false
-                    backgroundColor = .clear
-                }
+            /// A Boolean value that indicates whether the SwiftUI view ignores the safe area insets.
+            @available(macOS 11.0, *)
+            var ignoresSafeArea: Bool {
+                get { isMethodReplaced(#selector(getter: NSUIView.safeAreaInsets)) }
+                set { setSafeAreaInsets(newValue ? .zero : nil) }
+            }
+            
+            /// Sets the Boolean value that indicates whether the SwiftUI view ignores the safe area insets.
+            @discardableResult
+            @available(macOS 11.0, *)
+            func ignoresSafeArea(_ ignores: Bool) -> Self {
+                ignoresSafeArea = ignores
+                return self
             }
             
             /// Resizes the view’s frame so that it’s the size satisfies the constraints it holds.
             func sizeToFit() {
                 frame.size = fittingSize
             }
-            
-            /// A Boolean value that indicates whether the view should ignore save area insets.
-            @available(macOS 11.0, *)
-            var isSafeAreaInsetsDisabled: Bool {
-                get { isMethodReplaced(#selector(getter: NSUIView.safeAreaInsets)) }
-                set { setSafeAreaInsets((newValue == true) ? .zero : nil) }
-            }
-            
-            /// Sets the Boolean value that indicates whether the view should ignore save area insets.
-            @discardableResult
-            @available(macOS 11.0, *)
-            func ignoreSafeArea(_ ignores: Bool) -> Self {
-                isSafeAreaInsetsDisabled = ignores
-                return self
-            }
-            
-            @available(macOS 11.0, *)
-            internal func setSafeAreaInsets(_ newSafeAreaInsets: NSUIEdgeInsets?) {
-                resetMethod(#selector(getter: NSUIView.safeAreaInsets))
-                if let newSafeAreaInsets = newSafeAreaInsets {
-                    do {
-                        try replaceMethod(
-                            #selector(getter: NSUIView.safeAreaInsets),
-                            methodSignature: (@convention(c)  (AnyObject, Selector) -> (NSUIEdgeInsets)).self,
-                            hookSignature: (@convention(block)  (AnyObject) -> (NSUIEdgeInsets)).self) { store in {
-                               object in
-                               return newSafeAreaInsets
-                            }
-                       }
-                    } catch {
-                    // handle error
+        }
+fileprivate extension NSUIView {
+    @available(macOS 11.0, iOS 11.0, tvOS 11.0, *)
+    func setSafeAreaInsets(_ newSafeAreaInsets: NSUIEdgeInsets?) {
+        resetMethod(#selector(getter: NSUIView.safeAreaInsets))
+        if let newSafeAreaInsets = newSafeAreaInsets {
+            do {
+                try replaceMethod(
+                    #selector(getter: NSUIView.safeAreaInsets),
+                    methodSignature: (@convention(c)  (AnyObject, Selector) -> (NSUIEdgeInsets)).self,
+                    hookSignature: (@convention(block)  (AnyObject) -> (NSUIEdgeInsets)).self) { store in {
+                       object in
+                       return newSafeAreaInsets
                     }
-                }
+               }
+            } catch {
+                Swift.debugPrint(error)
             }
         }
+    }
+}
     #endif
 #endif
