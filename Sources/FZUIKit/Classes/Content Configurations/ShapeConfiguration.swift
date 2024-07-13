d734cf4d83c402acba64f9a77615b52bca041949
Florian Zand <florianzand@me.com>
2024-07-13 14:18:23 +0200

    Update ShapeConfiguration.swift

--- Sources/FZUIKit/Classes/Content Configurations/ShapeConfiguration.swift
+++ Sources/FZUIKit/Classes/Content Configurations/ShapeConfiguration.swift
@@ -22,13 +22,15 @@
     @available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0, *)
     public struct ShapeConfiguration: Hashable {
         /// The shape.
-        public var shape: (any SwiftUI.Shape)?
+        public var shape: (any SwiftUI.Shape)? {
+            didSet { name = UUID().uuidString }
+        }
 
         /// The margins for the shape.
         public var margins: NSDirectionalEdgeInsets = .zero
 
         /// A Boolean value that indicates whether the shape is inverted.
-        public var inverse: Bool = false
+        public var inverted: Bool = false
 
         var name: String = UUID().uuidString
 
@@ -42,14 +44,14 @@
          */
         public init(shape: (any SwiftUI.Shape)?, inverted: Bool = false, margins: NSDirectionalEdgeInsets = .zero) {
             self.shape = shape
-            inverse = inverted
+            self.inverted = inverted
             self.margins = margins
             name = UUID().uuidString
         }
 
         init(shape: (any SwiftUI.Shape)?, inverted: Bool = false, margins: NSDirectionalEdgeInsets = .zero, name: String? = nil) {
             self.shape = shape
-            inverse = inverted
+            self.inverted = inverted
             self.margins = margins
             self.name = name ?? UUID().uuidString
         }
@@ -64,6 +66,11 @@
         public static func circle(inverted: Bool = false, margins: NSDirectionalEdgeInsets = .zero) -> Self {
             Self(shape: SwiftUI.Circle(), inverted: inverted, margins: margins, name: "Circle\(inverted ? "Inverted" : "")")
         }
+        
+        /// A circle shape.
+        public static var circle: Self {
+            circle()
+        }
 
         /**
          A capsule shape.
@@ -75,6 +82,11 @@
         public static func capsule(inverted: Bool = false, margins: NSDirectionalEdgeInsets = .zero) -> Self {
             Self(shape: SwiftUI.Capsule(), inverted: inverted, margins: margins, name: "Capsule\(inverted ? "Inverted" : "")")
         }
+        
+        /// A capsule shape.
+        public static var capsule: Self {
+            capsule()
+        }
 
         /**
          A ellipse shape.
@@ -85,6 +97,11 @@
          */
         public static func ellipse(inverted: Bool = false, margins: NSDirectionalEdgeInsets = .zero) -> Self {
             Self(shape: SwiftUI.Ellipse(), inverted: inverted, margins: margins, name: "Ellipse\(inverted ? "Inverted" : "")")
+        }
+        
+        /// A ellipse shape.
+        public static var ellipse: Self {
+            ellipse()
         }
 
         /**
@@ -112,7 +129,7 @@
         }
 
         /**
-         A star rectangle shape.
+         A star shape.
 
          - Parameters:
             - points: The number of points of the star. The default value is `5`.
@@ -124,21 +141,23 @@
             let name = rounded ? "StarRounded\(points)\(inverted ? "Inverted" : "")" : "Star\(points)\(inverted ? "Inverted" : "")"
             return Self(shape: Star(points: points, rounded: rounded), inverted: inverted, margins: margins, name: name)
         }
+        
+        /// A star shape.
+        public static var star: Self {
+            star()
+        }
 
         /// No shape.
         public static var none: Self = .init(shape: nil, name: "None")
 
-        public static func == (lhs: FZUIKit.ShapeConfiguration, rhs: FZUIKit.ShapeConfiguration) -> Bool {
-            if lhs.name == rhs.name, lhs.margins == rhs.margins {
-                return true
-            }
-            return false
+        public static func == (lhs: ShapeConfiguration, rhs: ShapeConfiguration) -> Bool {
+            lhs.hashValue == rhs.hashValue
         }
 
         public func hash(into hasher: inout Hasher) {
-            hasher.combine(margins)
             hasher.combine(name)
-            hasher.combine(inverse)
+            hasher.combine(margins)
+            hasher.combine(inverted)
         }
     }
 
@@ -163,7 +182,7 @@
          */
         func configurate(using configuration: ShapeConfiguration) {
             if configuration.shape != nil {
-                if let shapeLayer = configuration.inverse ? inverseMask as? ShapeLayer : mask as? ShapeLayer {
+                if let shapeLayer = configuration.inverted ? inverseMask as? ShapeLayer : mask as? ShapeLayer {
                     shapeLayer.configuration = configuration
                 } else {
                     let shapeLayer = ShapeLayer()
@@ -171,10 +190,10 @@
                     shapeLayer.setupObserver(for: self)
                 }
             } else {
-                if let shapeLayer = configuration.inverse ? inverseMask as? ShapeLayer : mask as? ShapeLayer {
+                if let shapeLayer = configuration.inverted ? inverseMask as? ShapeLayer : mask as? ShapeLayer {
                     shapeLayer.removeFromSuperlayer()
                     frameObserver = nil
-                    if configuration.inverse {
+                    if configuration.inverted {
                         inverseMask = nil
                     } else {
                         mask = nil
@@ -191,15 +210,37 @@
 
     @available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0, *)
     struct ShapeContentView: View {
-        let configuration: ShapeConfiguration
-        var shape: AnyShape {
-            configuration.shape?.asAnyShape() ?? Rectangle().asAnyShape()
+        let shape: AnyShape
+        let color: Color
+        
+        public init(_ configuration: ShapeConfiguration, color: Color = .black) {
+            self.shape = configuration.shape?.asAnyShape() ?? Rectangle().asAnyShape()
+            self.color = color
         }
 
-        var body: some View {
-            shape.fill(.black)
+        public var body: some View {
+            shape.fill(color)
         }
     }
+
+@available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0, *)
+extension View {
+    /*
+    /**
+     Sets a clipping shape for this view.
+     
+     - Parameter configuration: The shape configuration.
+     */
+    @ViewBuilder
+    func clipShape(configuration: ShapeConfiguration?) -> some View {
+        if let shape = configuration?.shape?.asAnyShape() {
+            self.clipShape(shape)
+        } else {
+            self
+        }
+    }
+    */
+}
 
     @available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0, *)
     class ShapeLayer: CALayer {
@@ -210,14 +251,14 @@
             }
         }
 
-        lazy var hostingController = NSUIHostingController(rootView: ShapeContentView(configuration: configuration))
+        lazy var hostingController = NSUIHostingController(rootView: ShapeContentView(configuration))
 
         func setupObserver(for layer: CALayer) {
             layer.frameObserver = layer.observeChanges(for: \.frame, handler: { [weak self] old, new in
                 guard let self = self, old.size != new.size else { return }
                 self.frame.size = new.size
             })
-            if configuration.inverse {
+            if configuration.inverted {
                 layer.inverseMask = self
             } else {
                 layer.mask = self
@@ -233,7 +274,7 @@
         }
 
         func updateShape() {
-            hostingController.rootView = ShapeContentView(configuration: configuration)
+            hostingController.rootView = ShapeContentView(configuration)
             setNeedsLayout()
         }
 
