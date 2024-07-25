cf83947a318672c0400a997866e6de6fdca6e95b
Florian Zand <florianzand@me.com>
2024-07-25 09:23:15 +0200

    Update NSUIStackView+.swift

--- Sources/FZUIKit/Extensions/View/NSUIStackView+.swift
+++ Sources/FZUIKit/Extensions/View/NSUIStackView+.swift
@@ -27,6 +27,36 @@
         @objc open func removeAllArrangedSubviews() {
             arrangedSubviews.forEach { self.removeArrangedSubview($0) }
         }
+        
+        /**
+         Adds the specified views to the end of the arranged subviews list.
+         
+         - Parameter views: The views to add to the end of the arrangedSubviews array.
+         */
+        @objc open func addArrangedSubviews(_ views: [NSUIView]) {
+            views.forEach({ addArrangedSubview($0) })
+        }
+        
+        /**
+         Adds the provided views to the array of arranged subviews at the specified index.
+         
+         - Parameters:
+            - views: The views to be added to the array of arranged views managed by the stack.
+            - index: The index where the stack inserts the new view in its `arrangedSubviews` array.
+         */
+        @objc open func insertArrangedSubviews(_ views: [NSUIView], at index: Int) {
+            guard index >= 0, index < arrangedSubviews.count else { return }
+            views.reversed().forEach({ insertArrangedSubview($0, at: index) })
+        }
+        
+        /**
+         Removes the provided views from the stack’s array of arranged subviews.
+         
+         - Parameter views: The views to be removed from the array of views arranged by the stack.
+         */
+        @objc open func removeArrangedSubviews(_ views: [NSUIView]) {
+            views.filter({arrangedSubviews.contains($0)}).forEach({ removeArrangedSubview($0) })
+        }
 
         /// The array of views arranged by the stack view.
         @objc open var arrangedViews: [NSUIView] {
@@ -34,7 +64,9 @@
             set {
                 guard arrangedSubviews != newValue else { return }
                 removeAllArrangedSubviews()
-                newValue.forEach { self.addArrangedSubview($0) }
+                let diff = newValue.difference(to: arrangedSubviews)
+                diff.added.forEach({ addArrangedSubview($0) })
+                diff.removed.forEach({ removeArrangedSubview($0) })
             }
         }
         
