13d436593a71619f204d38bca6a17a51d6901ba3
Florian Zand <florianzand@me.com>
2024-08-16 21:58:28 +0200

    Update View+TextAlignment.swift

--- Sources/FZUIKit/Extensions/SwiftUI/View/View+TextAlignment.swift
+++ Sources/FZUIKit/Extensions/SwiftUI/View/View+TextAlignment.swift
@@ -16,37 +16,8 @@ public extension View {
         - autoWidth: A Boolean value that indicates whether the text should fit its width automatically.
      */
     func textAlignment(_ textAlignment: TextAlignment, autoWidth: Bool = true) -> some View {
-        modifier(TextAlignmentModifier(textAlignment: textAlignment, autoWidth: autoWidth))
-    }
-    
-    #if os(macOS) || os(iOS) || os(tvOS)
-    /**
-     Sets the alignment of a text.
-
-     - Parameters:
-        - textAlignment: The alignment of the text.
-        - autoWidth: A Boolean value that indicates whether the text should fit its width automatically.
-     */
-    func textAlignment(_ textAlignment: NSTextAlignment, autoWidth: Bool = true) -> some View {
-        modifier(TextAlignmentModifier(textAlignment: textAlignment.swiftUIMultiline, autoWidth: autoWidth))
-    }
-    #endif
-}
-
-struct TextAlignmentModifier: ViewModifier {
-    let textAlignment: TextAlignment
-    let autoWidth: Bool
-
-    init(textAlignment: TextAlignment, autoWidth: Bool = true) {
-        self.textAlignment = textAlignment
-        self.autoWidth = autoWidth
-    }
-
-    @ViewBuilder
-    func body(content: Content) -> some View {
-        content
-            .multilineTextAlignment(textAlignment)
-            .frame(maxWidth: autoWidth ? CGFloat.infinity : nil, alignment: textAlignment.alignment)
+        multilineTextAlignment(textAlignment)
+        .frame(maxWidth: autoWidth ? CGFloat.infinity : nil, alignment: textAlignment.alignment)
     }
 }
 
