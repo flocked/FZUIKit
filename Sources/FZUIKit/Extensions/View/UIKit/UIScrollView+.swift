543785f0a987dd02104941863c18d924cb556beb
Florian Zand <florianzand@me.com>
2024-08-30 23:43:56 +0200

    Update UIScrollView+.swift

--- Sources/FZUIKit/Extensions/View/UIKit/UIScrollView+.swift
+++ Sources/FZUIKit/Extensions/View/UIKit/UIScrollView+.swift
@@ -12,33 +12,23 @@ extension UIScrollView {
     /// Scale the content by the given amount and center the result on the given point.
     public func setZoomScale(_ scale: CGFloat, centeredAt point: CGPoint, animated: Bool = false) {
         if let view = delegate?.viewForZooming?(in: self) {
-            setZoomScale(scale, centeredAt: point, animated: animated, view: view)
+            setZoomScale(scale, center: convert(point, to: view), animated: animated)
         } else {
             let zoomFactor = 1.0 / zoomScale
             let scale = scale.clamped(to: minimumZoomScale...maximumZoomScale)
             let point = CGPoint(contentOffset.x * zoomFactor, contentOffset.y * zoomFactor)
-            
-            var destinationRect: CGRect = .zero
-            destinationRect.size.width = frame.width / scale
-            destinationRect.size.height = frame.height / scale
-            destinationRect.origin.x = point.x - destinationRect.width * 0.5
-            destinationRect.origin.y = point.y - destinationRect.height * 0.5
-            
-            zoom(to: destinationRect, animated: animated)
+            setZoomScale(scale, center: point, animated: animated)
         }
     }
     
-    func setZoomScale(_ scale: CGFloat, centeredAt point: CGPoint, animated: Bool, view: UIView) {
-        let contentCenter = convert(point, to: view)
-
-        let visibleWidth = frame.width / scale
-        let visibleHeight = frame.height / scale
-
-        let leftX = (contentCenter.x - (visibleWidth / 2))
-        let topY = (contentCenter.y - (visibleHeight / 2))
-
-        zoom(to: CGRect(x: leftX, y: topY, width: visibleWidth, height: visibleHeight), animated: animated)
-        }
+    func setZoomScale(_ scale: CGFloat, center: CGPoint, animated: Bool) {
+        var destinationRect: CGRect = .zero
+        destinationRect.size.width = frame.width / scale
+        destinationRect.size.height = frame.height / scale
+        destinationRect.origin.x = center.x - (destinationRect.width / 2.0)
+        destinationRect.origin.y = center.y - (destinationRect.height / 2.0)
+        zoom(to: destinationRect, animated: animated)
+    }
 }
 
 #endif
