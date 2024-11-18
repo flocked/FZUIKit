e146d5ec98a6fb4551aa3c88fd8d393b2b832c9e
Florian Zand <florianzand@me.com>
2024-11-18 06:11:10 +0100

    Update NSTextField+Editing.swift

--- Sources/FZUIKit/Extensions/View/AppKit/NSTextField/NSTextField+Editing.swift
+++ Sources/FZUIKit/Extensions/View/AppKit/NSTextField/NSTextField+Editing.swift
@@ -389,6 +389,7 @@
                             methodSignature: (@convention(c) (AnyObject, Selector, NSTextView, Selector) -> (Bool)).self,
                             hookSignature: (@convention(block) (AnyObject, NSTextView, Selector) -> (Bool)).self
                         ) { store in { object, textView, selector in
+                            /*
                             if let textField = object as? NSTextField {
                                 switch selector {
                                 case #selector(NSControl.cancelOperation(_:)):
@@ -422,6 +423,7 @@
                                 default: break
                                 }
                             }
+                             */
                             return store.original(object, #selector(NSTextViewDelegate.textView(_:doCommandBy:)), textView, selector)
                         }
                         }
