bdf2022c492514d88fa3e7a74685248163f9d95e
Florian Zand <florianzand@me.com>
2024-12-08 06:48:25 +0100

    Update Material+.swift

--- Sources/FZUIKit/Extensions/SwiftUI/Material+.swift
+++ Sources/FZUIKit/Extensions/SwiftUI/Material+.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 @available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
-extension Material: Equatable, CustomStringConvertible {
+extension Material: Equatable {
     public static func == (lhs: Material, rhs: Material) -> Bool {
-        lhs.description == rhs.description
+        lhs.id == rhs.id
     }
     
-    public var description: String {
+    var id: String {
         String(describing: self)
     }
 }
