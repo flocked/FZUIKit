e30e99f53646bc1bb35e56cd50514b635b7c2c6b
Florian Zand <florianzand@me.com>
2025-01-04 02:55:11 +0100

    Update NSUIImage+Shadow.swift

--- Sources/FZUIKit/Extensions/Image/NSUIImage+Shadow.swift
+++ Sources/FZUIKit/Extensions/Image/NSUIImage+Shadow.swift
@@ -40,7 +40,7 @@ public extension NSImage {
         return newImage
     }
 }
-#elseif os(macOS) || os(iOS) || os(tvOS)
+#elseif os(iOS) || os(tvOS)
 import UIKit
 
 public extension UIImage {
@@ -70,6 +70,9 @@ public extension UIImage {
         return image
     }
 }
+#endif
+
+#if os(macOS) || os(iOS) || os(tvOS)
 
 public extension CGImage {
     /// Returns a new image with the specified shadow configuraton.
