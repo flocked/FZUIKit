f6e3789077c20048beb33d3d0c55a7392bf00ade
Florian Zand <florianzand@me.com>
2025-03-10 12:16:33 +0100

    Update ImageView.swift

--- Sources/FZUIKit/Classes/View/AppKit/ImageView/ImageView.swift
+++ Sources/FZUIKit/Classes/View/AppKit/ImageView/ImageView.swift
@@ -67,6 +67,11 @@ open class ImageView: NSControl {
         set {  }
     }
     
+    open override class var cellClass: AnyClass? {
+        get { NSImageCell.self }
+        set { }
+    }
+    
     /// A Boolean value that indicates whether the images should be cropped to  non their non alpha area.
     open var cropImagesToNonTransparent: Bool = false {
         didSet {
