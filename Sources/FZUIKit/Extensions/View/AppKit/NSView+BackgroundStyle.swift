086d0bc8b89d3dc1ee30743508eae4242f85cfbb
Florian Zand <florianzand@me.com>
2025-03-10 12:10:01 +0100

    Update NSView+BackgroundStyle.swift

--- Sources/FZUIKit/Extensions/View/AppKit/NSView+BackgroundStyle.swift
+++ Sources/FZUIKit/Extensions/View/AppKit/NSView+BackgroundStyle.swift
@@ -42,21 +42,16 @@ extension NSView {
                 view.updateSymbolConfiguration()
             }
             subviews.forEach({  $0.setBackgroundStyle(backgroundStyle)  })
+            /*
             let selector = NSSelectorFromString("_setBackgroundStyleForSubtree:")
             guard let method = Self.instanceMethod(for: selector) else { return }
-            guard !NSView.isApplyingBackgroundStyle else { return }
-            NSView.isApplyingBackgroundStyle = true
             subviews.forEach({
                 if $0.responds(to: selector) {
                     unsafeBitCast(method, to: (@convention(c) (AnyObject, Selector, BackgroundStyle) -> ()).self)($0, selector, backgroundStyle)
                 }
             })
-            NSView.isApplyingBackgroundStyle = false
+             */
         }
-    }
-    static var isApplyingBackgroundStyle: Bool {
-        get { getAssociatedValue("isApplyingBackgroundStyle") ?? false }
-        set { setAssociatedValue(newValue, key: "isApplyingBackgroundStyle") }
     }
 }
 
