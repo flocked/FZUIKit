d41769816dbe51b2e952fa0f45780411897576d7
Florian Zand <florianzand@me.com>
2025-07-22 13:57:35 +0200

    Update NSView+Handlers.swift

--- Sources/FZUIKit/Extensions/View/AppKit/NSView+Handlers.swift
+++ Sources/FZUIKit/Extensions/View/AppKit/NSView+Handlers.swift
@@ -57,14 +57,12 @@ extension NSView {
         func getMenu() -> NSMenu? {
             guard let view = view, let event = NSEvent.current else { return nil }
             let location = event.location(in: view)
-            Swift.print("hitTest", view.hitTest(location) ?? "nil", view.hitTest(location)?.menu != nil, view.subviews.count, view.subviews.map({ type(of: $0) }))
             if let subview = view.hitTest(location) {
                 if event.type == .rightMouseDown {
                     subview.rightMouseDown(with: event)
                 } else if event.type == .rightMouseUp {
                     subview.rightMouseUp(with: event)
                 }
-                
                 guard let menu = subview.menu else { return nil }
                 return menu
             }
