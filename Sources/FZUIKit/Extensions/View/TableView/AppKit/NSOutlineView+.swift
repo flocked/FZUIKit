36a6d852a86660f19a3765a7e7431f04bf8409d7
Florian Zand <florianzand@me.com>
2025-07-30 05:34:43 +0200

    Update NSOutlineView+.swift

--- Sources/FZUIKit/Extensions/View/TableView/AppKit/NSOutlineView+.swift
+++ Sources/FZUIKit/Extensions/View/TableView/AppKit/NSOutlineView+.swift
@@ -250,19 +250,6 @@ extension NSOutlineView {
     }
     
     /**
-     Updates the data for the items you specify, preserving the existing row views and cells for the rows.
-
-     To update the contents of existing (including prefetched) cells without replacing them with new row views nad cells, use this method instead of ``AppKit/NSOutlineView/reloadItems(_:)``. For optimal performance, choose to reconfigure rows instead of reloading rows unless you have an explicit need to replace the existing row view or cells with new.
-     
-     - Parameter items: The items you want to update.
-     */
-    public func reconfigurateItems(_ items: [Any]) {
-        let rows = items.map({ row(forItem: $0) }).filter({ $0 != -1 })
-        guard !rows.isEmpty else { return }
-        reconfigureRows(at: IndexSet(rows))
-    }
-    
-    /**
      Reloads and redisplays the data for the given item.
      
      This method may cause the outline view to change its selection without calling the outlineViewSelectionDidChange(_:) delegate method.
