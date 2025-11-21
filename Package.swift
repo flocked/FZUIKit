a2fb810037b70251b8ad32d13b42cb736a79411b
Florian Zand <florianzand@me.com>
2025-11-21 06:09:52 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/flocked/FZSwiftUtils.git", branch: "main"),
+        .package(url: "https://github.com/flocked/FZSwiftUtils.git", from: "1.2.1"),
     ],
     targets: [
         .target(
