2f4956b3c9ae8c56c8969248844cf5769201a4a2
Florian Zand <florianzand@me.com>
2025-11-21 07:00:02 +0100

    Revert "Update Package.swift"
    This reverts commit 09ddedb81b45706bd3541665d318211bf94b68ab.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/flocked/FZSwiftUtils.git", from: "1.2.2"),
+        .package(url: "https://github.com/flocked/FZSwiftUtils.git", branch: "main"),
     ],
     targets: [
         .target(
