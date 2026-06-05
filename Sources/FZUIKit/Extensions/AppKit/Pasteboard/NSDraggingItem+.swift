b78e28b57d8ed25b5729c24e697d3b42672755d3
Florian Zand <florianzand@me.com>
2026-06-05 13:26:13 +0200

    Update NSDraggingItem+.swift

--- Sources/FZUIKit/Extensions/AppKit/Pasteboard/NSDraggingItem+.swift
+++ Sources/FZUIKit/Extensions/AppKit/Pasteboard/NSDraggingItem+.swift
@@ -9,7 +9,7 @@
 import AppKit
 import FZSwiftUtils
 
-extension NSDraggingItem {
+public extension NSDraggingItem {
     /**
      Sets the item’s dragging image.
      
@@ -17,7 +17,7 @@ extension NSDraggingItem {
         - image: The dragging image.
         - frame: The dragging image frame.
      */
-    public func setDraggingImage(_ image: NSImage, frame: CGRect? = nil) {
+    func setDraggingImage(_ image: NSImage, frame: CGRect? = nil) {
         setDraggingFrame(frame ?? image.size.rect, contents: image)
     }
     
@@ -28,14 +28,72 @@ extension NSDraggingItem {
         - image: The dragging image.
         - frame: The dragging image frame.
      */
-    public func setDraggingImage(_ image: CGImage, frame: CGRect? = nil) {
+    func setDraggingImage(_ image: CGImage, frame: CGRect? = nil) {
         setDraggingFrame(frame ?? image.size.rect, contents: image)
     }
     
     /// Sets the item’s dragging image using a rendered snapshot of the specified view.
-    public func setDraggingImage(view: NSView) {
+    func setDraggingImage(view: NSView) {
         setDraggingImage(view.renderedImage)
     }
+    
+    /**
+     Creates and returns a dragging item using the specified content and view.
+     
+     - Parameters:
+        - content: The content of the dragging item.
+        - View: he view associated with the dragging item. When beginning a dragging session, an image representation of the view is used as dragging image.
+     */
+    convenience init(_ content: PasteboardWriting, view: NSView) {
+        self.init(content.pasteboardWriting, view: view)
+    }
+    
+    /**
+     Creates and returns a dragging item using the specified content and view.
+     
+     - Parameters:
+        - content: The content of the dragging item.
+        - View: he view associated with the dragging item. When beginning a dragging session, an image representation of the view is used as dragging image.
+     */
+    convenience init(_ content: NSPasteboardWriting, view: NSView) {
+        self.init(pasteboardWriter: content)
+        self.view = view
+        NSView.swizzleBeginDraggingSession()
+    }
+    
+    /**
+     The view associated with the dragging item.
+     
+     When beginning a new dragging session, an image representation of the view is used as dragging image.
+     */
+    var view: NSView? {
+        get { getAssociatedValue("_view") }
+        set { setAssociatedValue(weak: newValue, key: "_view") }
+    }
+}
+
+fileprivate extension NSView {
+    static func swizzleBeginDraggingSession() {
+        guard !isMethodHooked(#selector(NSView.beginDraggingSession(with:event:source:))) else { return }
+        do {
+            try hook(all: #selector(NSView.beginDraggingSession(with:event:source:)), closure: { original, view, selector, items, event, source in
+                for item in items {
+                    guard let itemView = item.view else { continue }
+                    var components = item.imageComponents ?? []
+                    guard !components.contains(where: {$0.key == .view }) else { continue }
+                    components += .init(key: .view, image: itemView.renderedImage, frame: itemView.convert(itemView.bounds, to: view))
+                    item.imageComponentsProvider = { components }
+                }
+                return original(view, selector, items, event, source)
+            } as @convention(block) ((NSView, Selector, [NSDraggingItem], NSEvent, any NSDraggingSource) -> NSDraggingSession, NSView, Selector, [NSDraggingItem], NSEvent, any NSDraggingSource) -> NSDraggingSession)
+        } catch {
+            Swift.print(error)
+        }
+    }
+}
+
+fileprivate extension NSDraggingItem.ImageComponentKey {
+    static let view = Self.init("renderedView")
 }
 
 #endif
