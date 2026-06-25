5cb19ca21498dbbd209707566f08ae9f9ef1c80c
Florian Zand <florianzand@me.com>
2026-06-25 08:23:28 +0200

    Update QLThumbnailGenerator+.swift

--- Sources/FZUIKit/Extensions/Misc/QLThumbnailGenerator+.swift
+++ Sources/FZUIKit/Extensions/Misc/QLThumbnailGenerator+.swift
@@ -20,74 +20,78 @@ extension QLThumbnailGenerator {
 
      - Parameter requests: The thumbnail creation requests to cancel.
      */
-    public func cancel(_ requests: [QLThumbnailGenerator.Request]) {
-        requests.forEach { self.cancel($0) }
+    public func cancel<S: Sequence<Request>>(_ requests: S) {
+        requests.forEach({ cancel($0) })
     }
 
-    /// Cancels the generation of all thumbnail requests.
+    /// Cancels all pending thumbnail generation requests.
     public func cancelAllRequests() {
-        cancel(processingRequests)
-        cancel(pendingGenerationRequests)
-        cancel(preparingGenerationRequests)
+        pendingRequests.forEach({ cancel($0) })
     }
-
-    /// All thumbnail requests.
-    public var requests: [RequestStatus: [QLThumbnailGenerator.Request]] {
-        var requests: [RequestStatus: [QLThumbnailGenerator.Request]] = [:]
-        requests[.processing] = processingRequests
-        requests[.pendingCancelled] = pendingCancelledRequests
-        requests[.pendingGeneration] = pendingGenerationRequests
-        requests[.preparingGeneration] = preparingGenerationRequests
-        return requests
+    
+    /**
+     Cancels all pending requests that create thumbnails for the specified file URLs.
+     
+     - Parameter urls: The file URLs to cancel thumbnail generation requests for.
+     */
+    public func cancelRequests<S: Sequence<URL>>(for urls: S) {
+        for request in pendingRequests {
+            guard urls.contains(request.fileURL) else { continue }
+            cancel(request)
+        }
     }
-
-    /// The status of the request
-    public enum RequestStatus: Int {
-        /// The request is pending generation.
-        case pendingGeneration
-        /// The request is preparing for generation.
-        case preparingGeneration
-        /// The request is currently processed.
-        case processing
-        /// The request is pending cancellation.
-        case pendingCancelled
+    
+    /// All requests that are currently pending generation.
+    public var pendingRequests: [Request] {
+        processingRequests + pendingGenerationRequests + preparingGenerationRequests
     }
 
 
-
-    var processingRequests: [QLThumbnailGenerator.Request] {
-        ((value(forKeySafely: "requests") as? [UUID: QLThumbnailGenerator.Request]) ?? [:]).compactMap(\.value)
+    /// The requests currently being processed.
+    var processingRequests: [Request] {
+        (value(forKeySafely: "requests") as? [UUID: Request])?.map(\.value) ?? []
     }
 
-    var pendingCancelledRequests: [QLThumbnailGenerator.Request] {
-        ((value(forKeySafely: "pendingCancelledRequests") as? [UUID: QLThumbnailGenerator.Request]) ?? [:]).compactMap(\.value)
+    /// The requests pending cancellation.
+    var pendingCancelledRequests: [Request] {
+        (value(forKeySafely: "pendingCancelledRequests") as? [UUID: Request])?.map(\.value) ?? []
     }
 
-    var pendingGenerationRequests: [QLThumbnailGenerator.Request] {
-        ((value(forKeySafely: "pendingGenerationRequests") as? [UUID: QLThumbnailGenerator.Request]) ?? [:]).compactMap(\.value)
+    /// The requests pending generation.
+    var pendingGenerationRequests: [Request] {
+        (value(forKeySafely: "pendingGenerationRequests") as? [UUID: Request])?.map(\.value) ?? []
     }
 
-    var preparingGenerationRequests: [QLThumbnailGenerator.Request] {
-        ((value(forKeySafely: "preparingGenerationRequests") as? [UUID: QLThumbnailGenerator.Request]) ?? [:]).compactMap(\.value)
+    /// The request being prepared for generation.
+    var preparingGenerationRequests: [Request] {
+        (value(forKeySafely: "preparingGenerationRequests") as? [UUID: Request])?.map(\.value) ?? []
     }
 }
 
 extension QLThumbnailGenerator.Request {
-    /// The URL of the file for which you want to create a thumbnail.
+    /// The URL of the file to create a thumbnail for.
     public var fileURL: URL {
-        get { value(forKeySafely: "fileURL") as? URL ?? URL(fileURLWithPath: "No request URL") }
-        set { setValue(safely: newValue, forKey: "fileURL") }
+        get { value(forKeySafely: "fileURL") as? URL ?? .file("/") }
     }
 
     /**
-     Generates various thumbnail representations for the request and calls the update handler for each thumbnail representation.
+     Generates the best possible thumbnail representation for a file and calls a handler upon completion.
 
      - Parameter completionHandler: The completion handler to call when the thumbnail generation completes. It is always called when QLThumbnailGenerator finishes the generation of a requested thumbnail. The completion handler takes the following parameters:
         - thumbnail: The most representative version of the requested thumbnail or `nil` if `QLThumbnailGenerator` was unable to generate a thumbnail.
         - error: An error object indicating why the thumbnail generation failed, or `nil` if the thumbnail generation succeeded.
      */
-    public func generateBestRepresentation(completion completionHandler: @escaping (QLThumbnailRepresentation?, Error?) -> Void) {
+    public func generateBestRepresentation(completion completionHandler: @escaping (_ thumbnail: QLThumbnailRepresentation?, _ error: Error?) -> Void) {
         QLThumbnailGenerator.shared.generateBestRepresentation(for: self, completion: completionHandler)
+    }
+    
+    /**
+     Generates the best possible thumbnail representation for a file and calls a handler upon completion.
+
+     - Returns: The most representative version of the requested thumbnail.
+     */
+    public func generateBestRepresentation() async throws -> QLThumbnailRepresentation {
+        try await QLThumbnailGenerator.shared.generateBestRepresentation(for: self)
     }
 
     /**
@@ -95,18 +99,42 @@ extension QLThumbnailGenerator.Request {
 
      Use this method if you want to create a file icon or low-quality thumbnail quickly, and replace it with a higher quality thumbnail once it becomes available.
 
-     - Parameter updateHandler: The handler to call successively for each requested representation of a thumbnail. `QuickLookThumbnailing` calls the updateHandler in order of lower quality to higher quality thumbnail types. If a better quality thumbnail becomes available before a lower quality one, the framework may skip the call to the updateHandler for the lower quality thumbnail. You can rely on QuickLookThumbnailing to call the updateHandler at least once by the time it finishes the creation of thumbnails with either the best requested thumbnail, or an error object. The handler takes the following parameters:
+     - Parameter updateHandler: The handler to call successively for each requested representation of a thumbnail.  The handler is called in order of lower quality to higher quality thumbnail types. If a better quality thumbnail becomes available before a lower quality one, the framework may skip the call to the updateHandler for the lower quality thumbnail. You can rely on QuickLookThumbnailing to call the updateHandler at least once by the time it finishes the creation of thumbnails with either the best requested thumbnail, or an error object. The handler takes the following parameters:
         - thumbnail: A thumbnail that is successfully generated or `nil` if `QLThumbnailGenerator` is unable to generate a thumbnail.
         - type: The type of the generated thumbnail representation.
         - error: An error object indicating why the thumbnail generation failed, or `nil` if the thumbnail generation succeeded.
      */
-    public func generateRepresentations(update updateHandler: @escaping ((QLThumbnailRepresentation?, QLThumbnailRepresentation.RepresentationType, Error?) -> Void)) {
+    public func generateRepresentations(update updateHandler: @escaping ((_ thumbnail: QLThumbnailRepresentation?, _ type: QLThumbnailRepresentation.RepresentationType, _ error: Error?) -> Void)) {
         QLThumbnailGenerator.shared.generateRepresentations(for: self, update: updateHandler)
+    }
+    
+    /**
+     Saves a thumbnail for the request on disk at fileURL. The file saved at fileURL has to be deleted when it is not used anymore. This is primarily intended for file provider extensions which need to upload thumbnails and have a small memory limit.
+     
+     - Parameters:
+        - fileURL: The file url of the thumbnail to save.
+        - contentType: An image content type to save the thumbnail as, supported by CGImageDestination, such as [png](https://developer.apple.com/documentation/uniformtypeidentifiers/uttype-swift.struct/png) or [jpeg](https://developer.apple.com/documentation/uniformtypeidentifiers/uttype-swift.struct/jpeg).
+        - completionHandler: Always called when the thumbnail generation is over. Will contain an error if the thumbnail could not be successfully saved to disk at fileURL.
+     
+     */
+    public func saveBestRepresentation(to fileURL: URL, as contentType: UTType, completion completionHandler: @escaping @Sendable (Error?) -> Void) {
+        QLThumbnailGenerator.shared.saveBestRepresentation(for: self, to: fileURL, as: contentType, completion: completionHandler)
+    }
+    
+    /**
+     Saves a thumbnail for the request on disk at fileURL. The file saved at fileURL has to be deleted when it is not used anymore. This is primarily intended for file provider extensions which need to upload thumbnails and have a small memory limit.
+
+     - Parameters:
+        - fileURL: The file url of the thumbnail to save.
+        - contentType: An image content type to save the thumbnail as, supported by CGImageDestination, such as [png](https://developer.apple.com/documentation/uniformtypeidentifiers/uttype-swift.struct/png) or [jpeg](https://developer.apple.com/documentation/uniformtypeidentifiers/uttype-swift.struct/jpeg).
+     */
+    public func saveBestRepresentation(to fileURL: URL, as contentType: UTType) async throws {
+       try await QLThumbnailGenerator.shared.saveBestRepresentation(for: self, to: fileURL, as: contentType)
     }
 
     /// Cancels the thumbnail request.
     public func cancel() {
-        QLThumbnailGenerator.shared.cancel([self])
+        QLThumbnailGenerator.shared.cancel(self)
     }
 }
 
