3bb3cc0029a70da5c6de8e7fb13fd5682a062534
Florian Zand <florianzand@me.com>
2026-07-03 03:15:11 +0200

    Update NSSegmentedControl+Segment.swift

--- Sources/FZUIKit/Extensions/View/Control/NSSegmentedControl/NSSegmentedControl+Segment.swift
+++ Sources/FZUIKit/Extensions/View/Control/NSSegmentedControl/NSSegmentedControl+Segment.swift
@@ -408,44 +408,52 @@ public extension NSSegmentedControl {
     /// A function builder type that produces an array of segments.
     @resultBuilder
     enum Builder {
-        public static func buildBlock(_ block: [NSSegment]...) -> [NSSegment] {
-            block.flatMap { $0 }
-        }
-        
-        public static func buildOptional(_ item: NSSegment?) -> [NSSegment] {
-            item != nil ? [item!] : []
-        }
-        
-        public static func buildEither(first: [NSSegment]) -> [NSSegment] {
-            first
-        }
-        
-        public static func buildEither(second: [NSSegment]) -> [NSSegment] {
-            second
-        }
-        
-        public static func buildArray(_ components: [[NSSegment]]) -> [NSSegment] {
+        public static func buildBlock(_ components: [NSSegment]...) -> [NSSegment] {
             components.flatMap { $0 }
         }
-        
+
+        public static func buildExpression(_ expr: NSSegment) -> [NSSegment] {
+            [expr]
+        }
+
         public static func buildExpression(_ expr: NSSegment?) -> [NSSegment] {
             expr.map { [$0] } ?? []
         }
-        
+
+        public static func buildExpression(_ expr: [NSSegment]) -> [NSSegment] {
+            expr
+        }
+
         public static func buildExpression(_ expr: NSImage) -> [NSSegment] {
             [NSSegment(expr)]
         }
-        
+
         public static func buildExpression(_ expr: [NSImage]) -> [NSSegment] {
             expr.map { NSSegment($0) }
         }
-        
+
         public static func buildExpression(_ expr: String) -> [NSSegment] {
             [NSSegment(expr)]
         }
-        
+
         public static func buildExpression(_ expr: [String]) -> [NSSegment] {
             expr.map { NSSegment($0) }
+        }
+
+        public static func buildOptional(_ component: [NSSegment]?) -> [NSSegment] {
+            component ?? []
+        }
+
+        public static func buildEither(first component: [NSSegment]) -> [NSSegment] {
+            component
+        }
+
+        public static func buildEither(second component: [NSSegment]) -> [NSSegment] {
+            component
+        }
+
+        public static func buildArray(_ components: [[NSSegment]]) -> [NSSegment] {
+            components.flatMap { $0 }
         }
     }
 }
